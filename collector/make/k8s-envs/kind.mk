4756da497587680c1b2ad923a134e9b63379c9c5
John Cornish <jcornish@vmware.com>
2023-01-20 08:55:52 -0700

    tested corn/debug-ollector changes

--- collector/make/k8s-envs/kind.mk
+++ collector/make/k8s-envs/kind.mk
@@ -15,11 +15,6 @@ push-to-kind: container
 
 	@kind load docker-image $(PREFIX)/$(DOCKER_IMAGE):$(VERSION) --name kind
 
-cover-push-to-kind: cover-container
-	echo $(PREFIX)/$(DOCKER_IMAGE):$(VERSION)
-
-	@kind load docker-image $(PREFIX)/$(DOCKER_IMAGE):$(VERSION) --name kind
-
 delete-images-kind:
 	@docker exec -it kind-control-plane crictl rmi $(PREFIX)/$(DOCKER_IMAGE):$(VERSION) || true
 
