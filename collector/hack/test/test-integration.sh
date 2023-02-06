4f4c2fb1e715c74699e560ce6104eb48203114ad
Mark Michael <mamichael@vmware.com>
2023-02-06 13:11:36 -0800

    fix hpa metrics format and ci timing issue
    Co-authored-by: Yuqi Jin <jyuqi@vmware.com>

--- collector/hack/test/test-integration.sh
+++ collector/hack/test/test-integration.sh
@@ -114,6 +114,8 @@ function run_real_proxy() {
     additional_args="$additional_args -e $EXPERIMENTAL_FEATURES"
   fi
 
+  wait_for_cluster_resource_deleted namespace/$NS
+
   wait_for_cluster_ready
 
   "${SCRIPT_DIR}"/deploy.sh \
