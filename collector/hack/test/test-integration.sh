38d56b23683d05e627729c7ddaf84274c81bc314
Yuqi Jin <jyuqi@vmware.com>
2023-02-06 14:54:44 -0800

    Remove wait_for_cluster_resource_deleted
    Co-authored-by: Jerry Belmonte <bjerry@vmware.com>

--- collector/hack/test/test-integration.sh
+++ collector/hack/test/test-integration.sh
@@ -114,8 +114,6 @@ function run_real_proxy() {
     additional_args="$additional_args -e $EXPERIMENTAL_FEATURES"
   fi
 
-  wait_for_cluster_resource_deleted namespace/$NS
-
   wait_for_cluster_ready
 
   "${SCRIPT_DIR}"/deploy.sh \
