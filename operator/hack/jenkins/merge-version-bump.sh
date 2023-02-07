0b0adf8046342188c703eb60096fecde39bb95ef
wf-jenkins <34043090+wf-jenkins@users.noreply.github.com>
2023-02-08 04:45:55 +0530

    Bump collector version to 1.14.0 (#51)
    Co-authored-by: John Cornish <jcornish@vmware.com>
    Co-authored-by: Jerry Belmonte <bjerry@vmware.com>
    Co-authored-by: Jeremy Alvis <jalvis@vmware.com>
    Co-authored-by: Yuqi Jin <jyuqi@vmware.com>
    Co-authored-by: Priya Selvaganesan <pselvaganesa@vmware.com>
    Co-authored-by: Mark Michael <mamichael@vmware.com>

--- operator/hack/jenkins/merge-version-bump.sh
+++ operator/hack/jenkins/merge-version-bump.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(dirname "$0")"
 
 VERSION=$(cat ../../release/OPERATOR_VERSION)
-GIT_BUMP_BRANCH_NAME="bump-${VERSION}"
+GIT_BUMP_BRANCH_NAME="bump-operator-${VERSION}"
 git branch -D "$GIT_BUMP_BRANCH_NAME" &>/dev/null || true
 git checkout -b "$GIT_BUMP_BRANCH_NAME"
 
@@ -14,7 +14,7 @@ git push --force --set-upstream origin "${GIT_BUMP_BRANCH_NAME}"
 PR_URL=$(curl \
   -X POST \
   -H "Authorization: token ${GITHUB_TOKEN}" \
-  -d "{\"head\":\"${GIT_BUMP_BRANCH_NAME}\",\"base\":\"main\",\"title\":\"Bump version to ${VERSION}\"}" \
+  -d "{\"head\":\"${GIT_BUMP_BRANCH_NAME}\",\"base\":\"main\",\"title\":\"Bump operator version to ${VERSION}\"}" \
   https://api.github.com/repos/wavefrontHQ/observability-for-kubernetes/pulls |
   jq -r '.url')
 
@@ -25,4 +25,4 @@ curl \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
   "${PR_URL}/merge" \
-  -d "{\"commit_title\":\"Bump version to ${VERSION}\", \"merge_method\":\"squash\"}"
+  -d "{\"commit_title\":\"Bump operator version to ${VERSION}\", \"merge_method\":\"squash\"}"
