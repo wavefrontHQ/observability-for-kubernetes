258efffcc35958818cce4d00875f880d1e53ff68
Ginwoo Pak <pginwoo@vmware.com>
2023-02-23 13:27:25 -0700

    Finish release promotion script
    Co-authored-by: Priya Selvaganesan <pselvaganesa@vmware.com>
    Co-authored-by: Mark Michael <mamichael@vmware.com>

--- scripts/promote-release-images.sh
+++ scripts/promote-release-images.sh
@@ -37,6 +37,7 @@ done
 check_required_argument "${OPERATOR_BUMP_COMPONENT}" "-o <OPERATOR_BUMP_COMPONENT> is required"
 check_required_argument "${COLLECTOR_BUMP_COMPONENT}" "-c <COLLECTOR_BUMP_COMPONENT> is required"
 
+# Bump versions
 OLD_OPERATOR_VERSION=$(get_operator_version)
 OPERATOR_VERSION="$("${REPO_ROOT}"/scripts/get-bumped-version.sh -v "${OLD_OPERATOR_VERSION}" -s "${OPERATOR_BUMP_COMPONENT}")"
 echo "$OPERATOR_VERSION" >"${REPO_ROOT}"/operator/release/OPERATOR_VERSION
@@ -46,8 +47,22 @@ COLLECTOR_VERSION="$("${REPO_ROOT}"/scripts/get-bumped-version.sh -v "${OLD_COLL
 echo "$COLLECTOR_VERSION" >"${REPO_ROOT}"/collector/release/VERSION
 yq -i e ".data.collector |= \"$COLLECTOR_VERSION\"" "${REPO_ROOT}"/operator/config/manager/component_versions.yaml
 
+# Copy last tested wavefront-operator yaml from rc branch to dev-internal
 git show origin/rc:operator/wavefront-operator-main.yaml > ${REPO_ROOT}/operator/dev-internal/deploy/wavefront-operator.yaml
-OPERATOR_ALPHA_TAG=$(cat "${REPO_ROOT}"/operator/dev-internal/deploy/wavefront-operator.yaml | yq 'select(.metadata.name == "wavefront-controller-manager" and .kind == "Deployment" ) | .spec.template.spec.containers[0].image' | cut -d ':' -f2)
+
+# Promote alpha images to release versions
+OPERATOR_ALPHA_IMAGE=$(cat "${REPO_ROOT}"/operator/dev-internal/deploy/wavefront-operator.yaml | yq 'select(.metadata.name == "wavefront-controller-manager" and .kind == "Deployment" ) | .spec.template.spec.containers[0].image')
+OPERATOR_ALPHA_TAG=$(echo ${OPERATOR_ALPHA_IMAGE} | cut -d ':' -f2)
 COLLECTOR_ALPHA_TAG=$(cat "${REPO_ROOT}"/operator/dev-internal/deploy/wavefront-operator.yaml | yq 'select(.metadata.name == "wavefront-component-versions" ) | .data.collector')
 crane copy "projects.registry.vmware.com/tanzu_observability_keights_saas/kubernetes-operator:${OPERATOR_ALPHA_TAG}" "projects.registry.vmware.com/tanzu_observability_keights_saas/kubernetes-operator-snapshot:${OPERATOR_VERSION}"
 crane copy "projects.registry.vmware.com/tanzu_observability_keights_saas/kubernetes-collector:${COLLECTOR_ALPHA_TAG}" "projects.registry.vmware.com/tanzu_observability_keights_saas/kubernetes-collector-snapshot:${COLLECTOR_VERSION}"
+
+# Update wavefront-operator yaml in dev-internal with release versions
+sed -i.bak "s/collector: ${COLLECTOR_ALPHA_TAG}/collector: ${COLLECTOR_VERSION}/g" ${REPO_ROOT}/operator/dev-internal/deploy/wavefront-operator.yaml
+sed -i.bak "s#image: ${OPERATOR_ALPHA_IMAGE}#image: projects.registry.vmware.com/tanzu_observability/kubernetes-operator:${OPERATOR_VERSION}#g" ${REPO_ROOT}/operator/dev-internal/deploy/wavefront-operator.yaml
+rm -rf ${REPO_ROOT}/operator/dev-internal/deploy/wavefront-operator.yaml.bak
+
+# Promote dev-internal to top level
+pushd ${REPO_ROOT}
+  make promote-internal
+popd
