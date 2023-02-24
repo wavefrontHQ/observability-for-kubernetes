abfd3cd2d7c6011cda033f932bedf6f32a4c4a9a
Ginwoo Pak <pginwoo@vmware.com>
2023-02-24 10:47:11 -0700

    Remove stale install with elevated privileges
    Co-authored-by: Priya Selvaganesan <pselvaganesa@vmware.com>
    Co-authored-by: Glenn Oppegard <goppegard@vmware.com>

--- operator/hack/jenkins/setup-for-integration-test.sh
+++ operator/hack/jenkins/setup-for-integration-test.sh
@@ -76,7 +76,7 @@ echo after kustomize
 # crane
 #
 #  TODO: Delete this
-rm -rf /usr/local/bin/crane
+sudo rm -rf /usr/local/bin/crane
 if ! [ -x "$(command -v crane)" ]; then
   curl -H "Authorization: token ${GITHUB_TOKEN}" -L -s "https://github.com/google/go-containerregistry/releases/download/v0.13.0/go-containerregistry_Linux_x86_64.tar.gz" \
   | tar xz --to-stdout ./crane | sudo tee /usr/local/bin/crane >/dev/null
