524f424c5642cd3c935bb4516fd7115b3332c1e2
Priya Selvaganesan <pselvaganesa@vmware.com>
2023-02-24 10:57:30 -0700

    Get only crane folder from the tar
    Co-authored-by: Glenn Oppegard <goppegard@vmware.com>
    Co-authored-by: Ginwoo Pak <pginwoo@vmware.com>

--- operator/hack/jenkins/setup-for-integration-test.sh
+++ operator/hack/jenkins/setup-for-integration-test.sh
@@ -79,7 +79,8 @@ echo after kustomize
 sudo rm -rf /usr/local/bin/crane
 if ! [ -x "$(command -v crane)" ]; then
   curl -H "Authorization: token ${GITHUB_TOKEN}" -L -s "https://github.com/google/go-containerregistry/releases/download/v0.13.0/go-containerregistry_Linux_x86_64.tar.gz" \
-  | tar xz --to-stdout ./crane | sudo tee /usr/local/bin/crane >/dev/null
+  | tar --to-stdout -xz - crane \
+  | sudo tee /usr/local/bin/crane >/dev/null
   sudo chmod +x /usr/local/bin/crane
 fi
 echo after crane
