6db987d0c8f5d6c927110f0bb15a39a42cb5a7be
Jeremy Alvis <iplay88keys@users.noreply.github.com>
2023-02-27 11:24:37 -0700

    Updates for sparklines (#47)
    Co-authored-by: Ginwoo Pak <pginwoo@vmware.com>

--- scripts/dashboard-development/dashboards_validator_spec.rb
+++ scripts/dashboard-development/dashboards_validator_spec.rb
@@ -9,24 +9,24 @@ describe ColorChecker do
       cc = ColorChecker.new FakeDashboardIterator.new
 
       # Colors
-      expect(cc.line_with_closest_rgba('                "sparklineLineColor": "rgba(0,0,0,0.3)",', 'rgba(0,0,0,0.3)'))
-                                .to eq('                "sparklineLineColor": "rgba(0,0,0,1)",')
-      expect(cc.line_with_closest_rgba('"sparklineLineColor": "rgba(255,50,50,1)",', 'rgba(255,50,50,1)'))
-                                .to eq('"sparklineLineColor": "rgba(245,79,71,1)",')
-      expect(cc.line_with_closest_rgba('"sparklineLineColor": "rgba(0,255,0,1)",', 'rgba(0,255,0,1)'))
-                                .to eq('"sparklineLineColor": "rgba(133,200,26,1)",')
-      expect(cc.line_with_closest_rgba('"sparklineLineColor": "rgba(50,50,255,1)",', 'rgba(50,50,255,1)'))
-                                .to eq('"sparklineLineColor": "rgba(73,175,217,1)",')
+      expect(cc.line_with_closest_rgba('                "sparklineDisplayColor": "rgba(0,0,0,0.4)",', 'rgba(0,0,0,0.4)'))
+        .to eq('                "sparklineDisplayColor": "rgba(0,0,0,1)",')
+      expect(cc.line_with_closest_rgba('"sparklineDisplayColor": "rgba(255,50,50,1)",', 'rgba(255,50,50,1)'))
+        .to eq('"sparklineDisplayColor": "rgba(245,79,71,1)",')
+      expect(cc.line_with_closest_rgba('"sparklineDisplayColor": "rgba(0,255,0,1)",', 'rgba(0,255,0,1)'))
+        .to eq('"sparklineDisplayColor": "rgba(133,200,26,1)",')
+      expect(cc.line_with_closest_rgba('"sparklineDisplayColor": "rgba(50,50,255,1)",', 'rgba(50,50,255,1)'))
+        .to eq('"sparklineDisplayColor": "rgba(73,175,217,1)",')
 
       # Black and white
-      expect(cc.line_with_closest_rgba('"sparklineLineColor": "rgba(200,200,200,1)",', 'rgba(200,200,200,1)'))
-                                .to eq('"sparklineLineColor": "rgba(255,255,255,1)",')
-      expect(cc.line_with_closest_rgba('"sparklineLineColor": "rgba(50,50,50,1)",', 'rgba(50,50,50,1)'))
-                                .to eq('"sparklineLineColor": "rgba(0,0,0,1)",')
-      expect(cc.line_with_closest_rgba('"sparklineLineColor": "rgba(200,200,200,0.5)",', 'rgba(200,200,200,0.5)'))
-                                .to eq('"sparklineLineColor": "rgba(255,255,255,1)",')
-      expect(cc.line_with_closest_rgba('"sparklineLineColor": "rgba(50,50,50,0.5)",', 'rgba(50,50,50,0.5)'))
-                                .to eq('"sparklineLineColor": "rgba(0,0,0,1)",')
+      expect(cc.line_with_closest_rgba('"sparklineDisplayColor": "rgba(200,200,200,1)",', 'rgba(200,200,200,1)'))
+        .to eq('"sparklineDisplayColor": "rgba(255,255,255,1)",')
+      expect(cc.line_with_closest_rgba('"sparklineDisplayColor": "rgba(50,50,50,1)",', 'rgba(50,50,50,1)'))
+        .to eq('"sparklineDisplayColor": "rgba(0,0,0,1)",')
+      expect(cc.line_with_closest_rgba('"sparklineDisplayColor": "rgba(200,200,200,0.5)",', 'rgba(200,200,200,0.5)'))
+        .to eq('"sparklineDisplayColor": "rgba(255,255,255,1)",')
+      expect(cc.line_with_closest_rgba('"sparklineDisplayColor": "rgba(50,50,50,0.5)",', 'rgba(50,50,50,0.5)'))
+        .to eq('"sparklineDisplayColor": "rgba(0,0,0,1)",')
     end
   end
 
@@ -35,9 +35,41 @@ describe ColorChecker do
       cc = ColorChecker.new FakeDashboardIterator.new
 
       expect(cc.fix_hex_and_rgba('"sparklineDisplayColor": "rgba(250,250,250,0.9)",'))
-                          .to eq('"sparklineDisplayColor": "rgba(255,255,255,1)",')
+        .to eq('"sparklineDisplayColor": "rgba(255,255,255,1)",')
       expect(cc.fix_hex_and_rgba('"colors": ["#ffbf00"]'))
-                          .to eq('"colors": ["#f57600"]')
+        .to eq('"colors": ["#f57600"]')
+      expect(cc.fix_hex_and_rgba('"colors": ["#ffbf00"]'))
+        .to eq('"colors": ["#f57600"]')
+    end
+
+    it 'does not fix rgba for sparklineLineColor and sparklineFillColor' do
+      cc = ColorChecker.new FakeDashboardIterator.new
+
+      expect(cc.fix_hex_and_rgba('"sparklineLineColor": "rgba(250,250,250,0.9)",'))
+        .to eq('"sparklineLineColor": "rgba(250,250,250,0.9)",')
+      expect(cc.fix_hex_and_rgba('"sparklineFillColor": "rgba(250,250,250,0.9)",'))
+        .to eq('"sparklineFillColor": "rgba(250,250,250,0.9)",')
+    end
+
+    it 'does not change rgba if values are any of the valid colors' do
+      cc = ColorChecker.new FakeDashboardIterator.new
+
+      expect(cc.fix_hex_and_rgba("rgba(245,79,71,1)"))
+        .to eq("rgba(245,79,71,1)")
+      expect(cc.fix_hex_and_rgba("rgba(245,118,0,1)"))
+        .to eq("rgba(245,118,0,1)")
+      expect(cc.fix_hex_and_rgba("rgba(133,200,26,1)"))
+        .to eq("rgba(133,200,26,1)")
+      expect(cc.fix_hex_and_rgba("rgba(73,175,217,1)"))
+        .to eq("rgba(73,175,217,1)")
+      expect(cc.fix_hex_and_rgba("rgba(0,0,0,0.3)"))
+        .to eq("rgba(0,0,0,0.3)")
+      expect(cc.fix_hex_and_rgba("rgba(0,0,0,0.2)"))
+        .to eq("rgba(0,0,0,0.2)")
+      expect(cc.fix_hex_and_rgba("rgba(0,0,0,1)"))
+        .to eq("rgba(0,0,0,1)")
+      expect(cc.fix_hex_and_rgba("rgba(255,255,255,1)"))
+        .to eq("rgba(255,255,255,1)")
     end
   end
 
@@ -45,18 +77,18 @@ describe ColorChecker do
     it 'should fix raw r, g, or b, not convert it to closest other color' do
       cc = ColorChecker.new FakeDashboardIterator.new
 
-      expect(cc.get_rgb_color_diffs(Color.from_string('#700000'))[0][0].hex)
+      expect(cc.get_rgb_color_diffs(Color.from_string('#700000'), false)[0][0].hex)
         .to eq('#f54f47')
-      expect(cc.get_rgb_color_diffs(Color.from_string('#007000'))[0][0].hex)
+      expect(cc.get_rgb_color_diffs(Color.from_string('#007000'), false)[0][0].hex)
         .to eq('#85c81a')
-      expect(cc.get_rgb_color_diffs(Color.from_string('#000070'))[0][0].hex)
+      expect(cc.get_rgb_color_diffs(Color.from_string('#000070'), false)[0][0].hex)
         .to eq('#49afd9')
 
-      expect(cc.get_rgb_color_diffs(Color.from_string('#700000'))[0][1])
+      expect(cc.get_rgb_color_diffs(Color.from_string('#700000'), false)[0][1])
         .to eq(0.0)
-      expect(cc.get_rgb_color_diffs(Color.from_string('#007000'))[0][1])
+      expect(cc.get_rgb_color_diffs(Color.from_string('#007000'), false)[0][1])
         .to eq(0.0)
-      expect(cc.get_rgb_color_diffs(Color.from_string('#000070'))[0][1])
+      expect(cc.get_rgb_color_diffs(Color.from_string('#000070'), false)[0][1])
         .to eq(0.0)
     end
   end
