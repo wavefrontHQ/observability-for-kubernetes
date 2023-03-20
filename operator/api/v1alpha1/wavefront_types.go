2893e3a0584843c5efc5e0de6c80da0563cc440b
Anil Kodali <akodali@vmware.com>
2023-03-20 10:59:58 -0700

    Enhance crd description for tagGuaranteeList

--- operator/api/v1alpha1/wavefront_types.go
+++ operator/api/v1alpha1/wavefront_types.go
@@ -335,7 +335,7 @@ type Filters struct {
 	// List of metric patterns to allow
 	AllowList []string `json:"allowList,omitempty"`
 
-	// List of tags to keep. Supersedes all other filters.
+	// List of tags guaranteed to not be removed during kubernetes metrics collection. Supersedes all other collection filters. These tags are given priority if you hit the 20 tag limit.
 	TagGuaranteeList []string `json:"tagGuaranteeList,omitempty"`
 
 	// TODO: metricTagAllowList, metricTagDenyList, tagInclude, and tagExclude
