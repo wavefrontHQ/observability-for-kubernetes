066de5c5e53e191b409ac23fe497caafa886c760
Anil Kodali <akodali@vmware.com>
2023-04-17 15:55:55 -0700

    WIP: Add collector metric to test for expected point tags. The kuberenetes.cluster.pod.count metric doesn't pass the test yet.

--- operator/hack/test/test-wavefront-metrics.sh
+++ operator/hack/test/test-wavefront-metrics.sh
@@ -205,7 +205,7 @@ function main() {
   echo Expected kubernetes.observability.status tags
   cat $EXPECTED_TAGS_JSON
   exit_on_fail wait_for_query_match_tags "at(%22end%22%2C%202m%2C%20ts(%22kubernetes.observability.status%22%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22))" "${EXPECTED_TAGS_JSON}"
-  exit_on_fail wait_for_query_match_tags "ts(kubernetes.cluster.pod.count%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22)" "${EXPECTED_TAGS_JSON}"
+  exit_on_fail wait_for_query_match_tags "at(%22end%22%2C%202m%2C%20ts(%22ts(kubernetes.cluster.pod.count%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22))" "${EXPECTED_TAGS_JSON}"
   echo Expected collector version: $COLLECTOR_VERSION_IN_DECIMAL
   exit_on_fail wait_for_query_match_exact "ts(kubernetes.collector.version%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22%20AND%20installation_method%3D%22operator%22)" "${COLLECTOR_VERSION_IN_DECIMAL}"
   exit_on_fail wait_for_query_non_zero "ts(kubernetes.cluster.pod.count%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22)"
