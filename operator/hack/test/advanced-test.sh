46e31a92421f4e6d879422b63aa67011794278ff
Anil Kodali <akodali@vmware.com>
2023-04-19 12:34:37 -0700

    Comment out an advanced pre-processor test as we don't currently merge default and custom preprocessor rules.

--- operator/hack/test/advanced-test.sh
+++ operator/hack/test/advanced-test.sh
@@ -1,1 +1,2 @@
-exit_on_fail wait_for_query_match_exact "ts(kubernetes.collector.version%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22%20AND%20installation_method%3D%22operator%22%20AND%20processed%3D%22true%22)" "${COLLECTOR_VERSION_IN_DECIMAL}"
+# TODO: Uncomment this test as part of K8SSAAS-1743
+# exit_on_fail wait_for_query_match_exact "ts(kubernetes.collector.version%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22%20AND%20installation_method%3D%22operator%22%20AND%20processed%3D%22true%22)" "${COLLECTOR_VERSION_IN_DECIMAL}"
