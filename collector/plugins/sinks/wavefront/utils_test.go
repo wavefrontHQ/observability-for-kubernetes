2bfe28ee9f7e16716871c8e4816a0a969b39d253
Jeremy Alvis <jalvis@vmware.com>
2023-04-28 15:26:20 -0700

    Don't drop tags automatically that aren't prefixed with 'label.*'
    Co-authored-by: Mark Michael <mamichael@vmware.com>

--- collector/plugins/sinks/wavefront/utils_test.go
+++ collector/plugins/sinks/wavefront/utils_test.go
@@ -34,15 +34,15 @@ func TestCleanTags(t *testing.T) {
 		assert.True(t, len(tagLessThanMinLen) < minDedupeTagValueLen)
 
 		t.Run("when the tag names are different lengths", func(t *testing.T) {
-			actual := map[string]string{"long-tag-name": tagGreaterThanMinLen, "shrt-tg": tagGreaterThanMinLen}
+			actual := map[string]string{"label.long-tag-name": tagGreaterThanMinLen, "label.shrt-tg": tagGreaterThanMinLen}
 			cleanTags(actual, []string{}, 1)
-			assert.Equal(t, map[string]string{"shrt-tg": tagGreaterThanMinLen}, actual)
+			assert.Equal(t, map[string]string{"label.shrt-tg": tagGreaterThanMinLen}, actual)
 		})
 
 		t.Run("when the tag names of the same length", func(t *testing.T) {
-			actual := map[string]string{"dup2": tagGreaterThanMinLen, "dup1": tagGreaterThanMinLen}
+			actual := map[string]string{"label.dup2": tagGreaterThanMinLen, "label.dup1": tagGreaterThanMinLen}
 			cleanTags(actual, []string{}, 1)
-			assert.Equal(t, map[string]string{"dup1": tagGreaterThanMinLen}, actual)
+			assert.Equal(t, map[string]string{"label.dup1": tagGreaterThanMinLen}, actual)
 		})
 
 		t.Run("when the duplicated values are < min len characters", func(t *testing.T) {
@@ -52,15 +52,36 @@ func TestCleanTags(t *testing.T) {
 		})
 
 		t.Run("when the duplicated values are equal min len characters", func(t *testing.T) {
-			actual := map[string]string{"a-tag": tagEqualMinLen, "b-tag": tagEqualMinLen}
+			actual := map[string]string{"label.a-tag": tagEqualMinLen, "label.b-tag": tagEqualMinLen}
 			cleanTags(actual, []string{}, 1)
-			assert.Equal(t, map[string]string{"a-tag": tagEqualMinLen}, actual)
+			assert.Equal(t, map[string]string{"label.a-tag": tagEqualMinLen}, actual)
 		})
 
 		t.Run("when one of the duplicated values is in the tag include list", func(t *testing.T) {
 			actual := map[string]string{"nodename": "same-value", "hostname": "same-value"}
 			cleanTags(actual, []string{}, 1)
 			assert.Equal(t, map[string]string{"nodename": "same-value"}, actual)
+		})
+
+		t.Run("when duplicated values have non label.* tag names", func(t *testing.T) {
+			actual := map[string]string{"test-tag-name": "same-value", "label.test-tag-name": "same-value"}
+			cleanTags(actual, []string{}, 1)
+			assert.Equal(t, map[string]string{"test-tag-name": "same-value"}, actual)
+
+			actual = map[string]string{"label.test-tag-name": "same-value", "test-tag-name": "same-value"}
+			cleanTags(actual, []string{}, 1)
+			assert.Equal(t, map[string]string{"test-tag-name": "same-value"}, actual)
+
+			actual = map[string]string{
+				"test-tag-name":         "same-value",
+				"another-test-tag-name": "same-value",
+				"label.test-tag-name":   "same-value",
+			}
+			cleanTags(actual, []string{}, 2)
+			assert.Equal(t, map[string]string{
+				"test-tag-name":         "same-value",
+				"another-test-tag-name": "same-value",
+			}, actual)
 		})
 
 		t.Run("when under the max capacity", func(t *testing.T) {
@@ -121,71 +142,69 @@ func TestCleanTags(t *testing.T) {
 				"label.kubernetes.io/os":                       "linux",
 				"label.node.kubernetes.io/instance-type":       "e2-standard-2",
 				"label.topology.gke.io/zone":                   "us-central1-c",
-				"label.topology.kubernetes.io/region":          "us-central1"}
+				"label.topology.kubernetes.io/region":          "us-central1",
+			}
+
 			cleanTags(actual, []string{}, maxWavefrontTags)
-			assert.Equal(t, maxWavefrontTags, len(actual))
-			assert.Equal(t, expectedCleanedTags, actual)
+			require.Equal(t, expectedCleanedTags, actual)
+			require.Equal(t, maxWavefrontTags, len(actual))
 		})
 
 		t.Run("AKS example", func(t *testing.T) {
 			actual := map[string]string{
-				"cluster":                                "mamichael-aks-221116",
-				"node_role":                              "worker",
-				"nodename":                               "aks-agentpool-33708643-vmss000000",
-				"hostname":                               "aks-agentpool-33708643-vmss000000",
-				"type":                                   "node",
-				"pod_cidr":                               "10.96.2.0/24",
-				"internal_ip":                            "10.40.56.17",
-				"kernel_version":                         "5.10.127+",
-				"label.agentpool":                        "agentpool",
-				"label.beta.kubernetes.io/arch":          "amd64",
-				"label.beta.kubernetes.io/instance-type": "Standard_B4ms",
-				"label.beta.kubernetes.io/os":            "linux",
-				"label.failure-domain.beta.kubernetes.io/region":        "eastus",
-				"label.failure-domain.beta.kubernetes.io/zone":          "0",
-				"label.kubernetes.azure.com/agentpool":                  "agentpool",
-				"label.kubernetes.azure.com/cluster":                    "MC_K8sSaaS_k8po-ci_eastus",
-				"label.kubernetes.azure.com/kubelet-identity-client-id": "50903262-46af-4c4d-b1c7-440985c16284",
+				"cluster":                              "jalvis-aks-230428",
+				"cluster_uuid":                         "9579295d-eddf-40df-95dd-bf4b528b6d05",
+				"host_id":                              "",
+				"hostname":                             "aks-agentpool-18535100-vmss000000",
+				"node_role":                            "worker",
+				"nodename":                             "aks-agentpool-18535100-vmss000000",
+				"resource_id":                          "/",
+				"type":                                 "node",
+				"test":                                 "node",
+				"label.agentpool":                      "agentpool",
+				"label.beta.kubernetes.io/arch":        "amd64",
+				"label.beta.kubernetes.io/os":          "linux",
+				"label.kubernetes.azure.com/agentpool": "agentpool",
+				"label.kubernetes.azure.com/cluster":   "MC_K8sSaaS_k8s-upgrade-test_westus2",
+				"label.kubernetes.azure.com/kubelet-identity-client-id": "80e47095-2878-4d98-9758-9dbfab610463",
 				"label.kubernetes.azure.com/mode":                       "system",
-				"label.kubernetes.azure.com/node-image-version":         "AKSUbuntu-1804gen2containerd-2022.08.10",
+				"label.kubernetes.azure.com/node-image-version":         "AKSUbuntu-2204gen2containerd-202304.10.0",
 				"label.kubernetes.azure.com/os-sku":                     "Ubuntu",
 				"label.kubernetes.azure.com/role":                       "agent",
 				"label.kubernetes.azure.com/storageprofile":             "managed",
 				"label.kubernetes.azure.com/storagetier":                "Premium_LRS",
-				"label.kubernetes.io/arch":                              "amd64",
-				"label.kubernetes.io/hostname":                          "aks-agentpool-33708643-vmss000000",
+				"label.kubernetes.io/hostname":                          "aks-agentpool-18535100-vmss000000",
 				"label.kubernetes.io/os":                                "linux",
 				"label.kubernetes.io/role":                              "agent",
 				"label.node-role.kubernetes.io/agent":                   "",
-				"label.node.kubernetes.io/instance-type":                "Standard_B4ms",
+				"label.node.kubernetes.io/instance-type":                "Standard_DS2_v2",
 				"label.storageprofile":                                  "managed",
 				"label.storagetier":                                     "Premium_LRS",
-				"resource_id":                                           "/",
-				"label.topology.disk.csi.azure.com/zone":                "",
-				"label.topology.kubernetes.io/region":                   "eastus",
-				"label.topology.kubernetes.io/zone":                     "0",
+				"label.topology.disk.csi.azure.com/zone":                "westus2-1",
+				"label.topology.kubernetes.io/region":                   "westus2",
+				"label.topology.kubernetes.io/zone":                     "westus2-1",
 			}
 
 			expected := map[string]string{
-				"cluster":         "mamichael-aks-221116",
-				"node_role":       "worker",
-				"nodename":        "aks-agentpool-33708643-vmss000000",
-				"type":            "node",
-				"pod_cidr":        "10.96.2.0/24",
-				"internal_ip":     "10.40.56.17",
-				"kernel_version":  "5.10.127+",
+				"cluster":         "jalvis-aks-230428",
+				"cluster_uuid":    "9579295d-eddf-40df-95dd-bf4b528b6d05",
 				"label.agentpool": "agentpool",
-				"label.kubernetes.azure.com/node-image-version": "AKSUbuntu-1804gen2containerd-2022.08.10",
-				"label.kubernetes.azure.com/mode":               "system",
-				"label.kubernetes.azure.com/os-sku":             "Ubuntu",
-				"label.kubernetes.io/arch":                      "amd64",
-				"label.kubernetes.io/os":                        "linux",
-				"label.kubernetes.io/role":                      "agent",
-				"label.node.kubernetes.io/instance-type":        "Standard_B4ms",
-				"label.storageprofile":                          "managed",
-				"label.storagetier":                             "Premium_LRS",
-				"label.topology.kubernetes.io/region":           "eastus",
-				"label.topology.kubernetes.io/zone":             "0",
+				"label.kubernetes.azure.com/kubelet-identity-client-id": "80e47095-2878-4d98-9758-9dbfab610463",
+				"label.kubernetes.azure.com/mode":                       "system",
+				"label.kubernetes.azure.com/node-image-version":         "AKSUbuntu-2204gen2containerd-202304.10.0",
+				"label.kubernetes.azure.com/os-sku":                     "Ubuntu",
+				"label.kubernetes.io/hostname":                          "aks-agentpool-18535100-vmss000000",
+				"label.kubernetes.io/os":                                "linux",
+				"label.kubernetes.io/role":                              "agent",
+				"label.node.kubernetes.io/instance-type":                "Standard_DS2_v2",
+				"label.storageprofile":                                  "managed",
+				"label.storagetier":                                     "Premium_LRS",
+				"label.topology.kubernetes.io/region":                   "westus2",
+				"label.topology.kubernetes.io/zone":                     "westus2-1",
+				"node_role":                                             "worker",
+				"nodename":                                              "aks-agentpool-18535100-vmss000000",
+				"test":                                                  "node",
+				"type":                                                  "node",
 			}
 
 			cleanTags(actual, []string{}, maxWavefrontTags)
