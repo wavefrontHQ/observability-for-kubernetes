895a2aa9d141a2f31ca97370680963ebb7418ec3
mamichael <mamichael@vmware.com>
2023-05-02 09:35:30 -0600

    Adds wait for ns to be deleted check in collector run real proxy script

--- collector/hack/test/test-integration.sh
+++ collector/hack/test/test-integration.sh
@@ -60,9 +60,8 @@ function run_real_proxy() {
     additional_args="$additional_args -e $EXPERIMENTAL_FEATURES"
   fi
 
-  echo "**** $NS *****"
-  wait_for_cluster_resource_deleted namespace/$NS
 
+  wait_for_cluster_resource_deleted namespace/$NS
   wait_for_cluster_ready
 
   "${SCRIPT_DIR}"/deploy.sh \
