4a18661fade265e0a40853bf34cf883262c168e2
Jerry Belmonte <bjerry@vmware.com>
2023-05-09 17:43:46 -0700

    Update kustomization version when releasing (#181)

--- operator/hack/jenkins/merge-version-bump.sh
+++ operator/hack/jenkins/merge-version-bump.sh
@@ -3,7 +3,7 @@ set -eou pipefail
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 source "${REPO_ROOT}/scripts/k8s-utils.sh"
-cd "$(dirname "$0")"
+cd "$REPO_ROOT"
 
 VERSION=$(cat ${REPO_ROOT}/operator/release/OPERATOR_VERSION)
 GIT_BUMP_BRANCH_NAME="release-operator-${VERSION}"
@@ -11,9 +11,9 @@ git branch -D "$GIT_BUMP_BRANCH_NAME" &>/dev/null || true
 git checkout -b "$GIT_BUMP_BRANCH_NAME"
 
 # Add changes from promoting dev-internal folder
-pushd "$REPO_ROOT"
-  git add deploy/ docs/ README.md
-popd
+sed -i.bak "s%newTag:.*$%newTag: ${VERSION}%" operator/config/manager/kustomization.yaml
+rm -f operator/config/manager/kustomization.yaml.bak
+git add deploy/ docs/ README.md operator/config/manager/kustomization.yaml
 git commit -am "Release operator version: ${VERSION}"
 git push --force --set-upstream origin "${GIT_BUMP_BRANCH_NAME}"
 
