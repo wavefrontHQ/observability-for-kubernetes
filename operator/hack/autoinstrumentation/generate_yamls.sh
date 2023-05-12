d6c021264eebc71d713920d780958b6bfdfe6740
Glenn Oppegard <goppegard@vmware.com>
2023-05-12 14:29:31 -0700

    improved generate_yamls.sh
    Co-authored-by: Ginwoo Pak <pginwoo@vmware.com>

--- operator/hack/autoinstrumentation/generate_yamls.sh
+++ operator/hack/autoinstrumentation/generate_yamls.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
+REPO_ROOT=$(git rev-parse --show-toplevel)
 rm -rf pixie_yamls
 
-px deploy --extract_yaml . --deploy_key foobar --use_etcd_operator=false
+px deploy --extract_yaml . --deploy_key replace_me --use_etcd_operator=false --cluster_name=replace_me
 
 tar -xvf yamls.tar
 
@@ -12,11 +13,14 @@ pushd pixie_yamls
 mkdir splits
 
 # Split resources into their own yaml files
-files_to_apply=(00_secrets.yaml 01_nats.yaml 04_vizier_persistent.yaml 06_vizier_persistent_ap.yaml)
+files_to_apply=(00_secrets.yaml 01_nats.yaml 04_vizier_persistent.yaml)
 cat "${files_to_apply[@]}" | csplit -n 3 -f 'splits/pixie-' - '/^---$/' "{$(($(cat "${files_to_apply[@]}" | grep -c '^\-\-\-$') - 2))}"
 
 # Remove duplicate resources
-fdupes -f splits | grep -v '^$' | xargs rm -v
+duplicates=$(fdupes -f splits)
+if [[ $duplicates != "" ]]; then
+  echo "$duplicates" | grep -v '^$' | xargs rm -fv
+fi
 
 # rename everything to a yaml file
 original_file_names=($(echo splits/pixie-*))
@@ -25,7 +29,7 @@ mkdir -p splits/secrets
 for index in "${!original_file_names[@]}"; do
   original_file_name="${original_file_names[$index]}"
   kind="$(grep '^kind:' "$original_file_name" | cut -d':' -f2 | xargs | tr '[:upper:]' '[:lower:]')"
-  name="$(grep '^  name:' "$original_file_name" | cut -d':' -f2  | xargs)"
+  name="$(grep '^  name:' "$original_file_name" | cut -d':' -f2  | xargs | tr '[:blank:]' '_')"
   if [[ "$kind" =~ role ]]; then
     num="$(printf "%02d" "$(find splits/roles -maxdepth 1 -name '*.yaml' | wc -l | xargs)")"
     new_file="splits/roles/$num-$kind-$name.yaml"
@@ -40,4 +44,20 @@ for index in "${!original_file_names[@]}"; do
   rm "$original_file_name"
 done
 
+git rm -rf "${REPO_ROOT}/operator/config/rbac/components/pixie/*.yaml"
+mkdir -p "${REPO_ROOT}/operator/config/rbac/components/pixie"
+cp splits/roles/*.yaml "${REPO_ROOT}/operator/config/rbac/components/pixie"
+git add "${REPO_ROOT}/operator/config/rbac/components/pixie"
+
+git rm -rf "${REPO_ROOT}/operator/deploy/internal/pixie/*.yaml"
+mkdir -p "${REPO_ROOT}/operator/deploy/internal/pixie"
+cp splits/secrets/*.yaml "${REPO_ROOT}/operator/deploy/internal/pixie"
+cp splits/*.yaml "${REPO_ROOT}/operator/deploy/internal/pixie"
+
+sed -i '' 's/  PL_CLUSTER_NAME: "replace_me"/  PL_CLUSTER_NAME: {{ .ClusterName }}/' "${REPO_ROOT}/operator/deploy/internal/pixie/00-configmap-pl-cloud-config.yaml"
+sed -i '' 's/  deploy-key: "replace_me"/  deploy-key: {{ .Experimental.AutoInstrumentation.DeployKey }}/' "${REPO_ROOT}/operator/deploy/internal/pixie/01-secret-pl-deploy-secrets.yaml"
+git add "${REPO_ROOT}/operator/deploy/internal/pixie"
+
 popd
+
+rm -rf yamls.tar pixie_yamls
