cfd57a1e931b27c832f7d1c86165acbaaac0f0d0
Glenn Oppegard <goppegard@vmware.com>
2023-06-05 15:31:18 -0700

    fixed the cluster uuid env variable in cluster collector
    Co-authored-by: Ginwoo Pak <pginwoo@vmware.com>

--- collector/plugins/sinks/manager.go
+++ collector/plugins/sinks/manager.go
@@ -127,7 +127,7 @@ func (sm *sinkManager) ExportEvent(event *events.Event) {
 			log.WithField("name", sh.sink.Name()).Debug("Pushing Events to sink")
 			select {
 			case sh.eventBatchChannel <- event:
-				log.WithField("name", sh.sink.Name()).Debug("Events push complete")
+				log.WithField("name", sh.sink.Name()).Info("Events push complete")
 				// everything ok
 			case <-time.After(sm.exportDataTimeout):
 				sinkTimeouts.Inc(1)
