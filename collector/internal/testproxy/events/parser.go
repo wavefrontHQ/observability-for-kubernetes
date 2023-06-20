ef3ec1cdc8074637675f83ef59217db2000d3cfc
Devon Warshaw <warshawd@vmware.com>
2023-06-20 15:42:03 -0700

    K8SSAAS-2027: Event line parser finished for integration tests
    Co-authored-by: Matthew Conger-Eldeen <matthewco@vmware.com>

--- collector/internal/testproxy/events/parser.go
+++ collector/internal/testproxy/events/parser.go
@@ -1,133 +1,25 @@
 package events
 
-import (
-	"errors"
-	"strings"
-)
-
 type Event struct {
 	Name        string
 	Start       string
 	End         string
 	Annotations map[string]string
+	Tags        map[string]string
 }
 
 func Parse(line string) (*Event, error) {
-	var event Event
-
-	const eventPrefix = "@Event "
-	if !strings.HasPrefix(line, eventPrefix) {
-		return nil, errors.New("line does not start with @Event")
+	g := &EventGrammar{Buffer: line}
+	g.Init()
+	if err := g.Parse(); err != nil {
+		return nil, err
 	}
-	line = line[len(eventPrefix):]
-	startMillis, line := parseMillis(line)
-	event.Start = startMillis
-	if len(startMillis) == 0 {
-		return nil, errors.New("invalid start time")
-	}
-	endMillis, line := parseMillis(line)
-	event.End = endMillis
-	if len(endMillis) == 0 {
-		return nil, errors.New("invalid end time")
-	}
-	name, line := endsWith(isLower, isSpace, line)
-	event.Name = name
-	if len(name) == 0 {
-		return nil, errors.New("invalid name")
-	}
-	event.Annotations = map[string]string{}
-	var annotationKey string
-	var annotationValue string
-	for len(line) > 0 {
-		annotationKey, line = endsWith(isLower, isEqual, line)
-		if len(annotationKey) == 0 {
-			return nil, errors.New("invalid annotation key")
-		}
-		annotationValue, line = surroundedBy(isAnnotationValue, isQuote, line)
-		if len(annotationValue) == 0 {
-			return nil, errors.New("invalid annotation value")
-		}
-		event.Annotations[annotationKey] = annotationValue
-		if len(line) > 0 && isSpace([]rune(line)[0]) {
-			line = line[1:]
-		}
-	}
-	return &event, nil
-}
-
-func isAnnotationValue(r rune) bool {
-	return !isQuote(r)
-}
-
-func parseMillis(line string) (millis string, rest string) {
-	return endsWith(isNumber, isSpace, line)
-}
-
-func parseToken(isToken func(rune) bool, line string) (token string, rest string) {
-	for _, r := range line {
-		if isToken(r) {
-			token += string(r)
-		} else {
-			break
-		}
-	}
-	return token, line[len(token):]
-}
-
-// Returns token and rest of line
-func surroundedBy(isToken func(rune) bool, isBoundary func(rune) bool, line string) (string, string) {
-	if len(line) == 0 {
-		return "", line
-	}
-	runes := []rune(line)
-	if !isBoundary(runes[0]) {
-		return "", line
-	}
-
-	var token string
-	runesConsumed := 2 // boundary characters
-	for i, r := range runes[1:] {
-		if isBoundary(r) {
-			if runes[i] == '\\' {
-				token = token[:len(token)-1]
-			} else {
-				break
-			}
-		}
-		runesConsumed += 1
-		token += string(r)
-	}
-	return token, line[runesConsumed:]
-}
-
-func endsWith(isToken func(rune) bool, isBoundary func(rune) bool, line string) (string, string) {
-	token, rest := parseToken(isToken, line)
-	if len(rest) == 0 {
-		return "", line
-	}
-	runes := []rune(rest)
-	if !isBoundary(runes[0]) {
-		return "", line
-	}
-	return token, string(runes[1:])
-}
-
-func isQuote(r rune) bool {
-	return r == '"'
-}
-
-func isEqual(r rune) bool {
-	return r == '='
-}
-
-func isLower(r rune) bool {
-	return 'a' <= r && r <= 'z'
-}
-
-func isNumber(r rune) bool {
-	return '0' <= r && r <= '9'
-}
-
-func isSpace(r rune) bool {
-	return r == ' '
+	g.Execute()
+	return &Event{
+		Name:        g.Name,
+		Start:       g.StartMillis,
+		End:         g.EndMillis,
+		Annotations: g.Annotations,
+		Tags:        g.Tags,
+	}, nil
 }
--- collector/internal/testproxy/events/parser_test.go
+++ collector/internal/testproxy/events/parser_test.go
@@ -9,32 +9,32 @@ import (
 func TestParse(t *testing.T) {
 	t.Run("rejects events that don't start with @Event", func(t *testing.T) {
 		_, err := Parse("@")
-		require.ErrorContains(t, err, "line does not start with @Event")
+		require.ErrorContains(t, err, "parse error near")
 	})
 
 	t.Run("rejects events that don't have a valid millisecond start time", func(t *testing.T) {
 		_, err := Parse("@Event 123abc")
-		require.ErrorContains(t, err, "invalid start time")
+		require.ErrorContains(t, err, "parse error near")
 	})
 
 	t.Run("rejects events that don't have a valid millisecond end time", func(t *testing.T) {
 		_, err := Parse("@Event 123 456def")
-		require.ErrorContains(t, err, "invalid end time")
+		require.ErrorContains(t, err, "parse error near")
 	})
 
 	t.Run("rejects events with bad names", func(t *testing.T) {
-		_, err := Parse("@Event 123 456 /name")
-		require.ErrorContains(t, err, "invalid name")
+		_, err := Parse("@Event 123 456 %name")
+		require.ErrorContains(t, err, "parse error near")
 	})
 
 	t.Run("rejects events with invalid annotation key", func(t *testing.T) {
-		_, err := Parse("@Event 123 456 name _annotation")
-		require.ErrorContains(t, err, "invalid annotation key")
+		_, err := Parse("@Event 123 456 name %annotation")
+		require.ErrorContains(t, err, "parse error near")
 	})
 
 	t.Run("rejects events with invalid annotation value", func(t *testing.T) {
 		_, err := Parse("@Event 123 456 name annotation=value")
-		require.ErrorContains(t, err, "invalid annotation value")
+		require.ErrorContains(t, err, "parse error near")
 	})
 
 	t.Run("parses events successfully", func(t *testing.T) {
@@ -47,7 +47,7 @@ func TestParse(t *testing.T) {
 		require.Equal(t, "name", event.Name)
 	})
 
-	t.Run("parses out event range", func(t *testing.T) {
+	t.Run("parses out event timestamps", func(t *testing.T) {
 		event, _ := Parse("@Event 123 456 name annotation=\"value\"")
 		require.Equal(t, "123", event.Start, "start millis")
 		require.Equal(t, "456", event.End, "end millis")
@@ -65,19 +65,45 @@ func TestParse(t *testing.T) {
 	})
 
 	t.Run("annotation values can have any character unescaped (except quotes)", func(t *testing.T) {
-		event, _ := Parse("@Event 123 456 name someannotation=\"!@#$%^&*()abcd1231241234\"")
-		require.Equal(t, "!@#$%^&*()abcd1231241234", event.Annotations["someannotation"])
+		event, _ := Parse("@Event 123 456 name someannotation=\"!@#$%^&*()abcd1231241234_+=:<>,.?/\"")
+		require.Equal(t, "!@#$%^&*()abcd1231241234_+=:<>,.?/", event.Annotations["someannotation"])
 	})
 
 	t.Run("parses escaped quotes in annotations properly", func(t *testing.T) {
-		event, _ := Parse(`@Event 123 456 name someannotation="foo\"bar"`)
-		require.Equal(t, `foo"bar`, event.Annotations["someannotation"])
+		event, _ := Parse(`@Event 123 456 name someannotation="foo: \"bar\""`)
+		require.Equal(t, `foo: "bar"`, event.Annotations["someannotation"])
 	})
 
-	t.Run("fails to parse annotations which do not end with a quote", func(t *testing.T) {
+	t.Run("rejects annotations which do not end with a quote", func(t *testing.T) {
 		_, err := Parse(`@Event 123 456 name someannotation="foo`)
-		require.ErrorContains(t, err, "invalid annotation value")
+		require.ErrorContains(t, err, "parse error near")
 	})
 
-	// TODO support tags
+	t.Run("rejects annotations which do not end with a quote", func(t *testing.T) {
+		_, err := Parse(`@Event 123 456 name someannotation="fooannother="bar"`)
+		require.ErrorContains(t, err, "parse error near")
+	})
+
+	t.Run("parses tags separately", func(t *testing.T) {
+		event, err := Parse(`@Event 123 456 name someannotation="foo" tag="\"tagKey\": \"tagValue\""`)
+
+		require.NoError(t, err)
+
+		require.Empty(t, event.Annotations["tag"])
+		require.Equal(t, "tagValue", event.Tags["tagKey"])
+	})
+
+	t.Run("parses complex tag values", func(t *testing.T) {
+		event, err := Parse(`@Event 123 456 name someannotation="foo" tag="\"tagKey\": \"!@#$%^&*()abcd1231241234_+=:<>,.?/\""`)
+
+		require.NoError(t, err)
+
+		require.Equal(t, "!@#$%^&*()abcd1231241234_+=:<>,.?/", event.Tags["tagKey"])
+	})
+
+	t.Run("parses complex tag values", func(t *testing.T) {
+		_, err := Parse(`@Event 123 456 name someannotation="foo" tag="\"tagKey\": \""\""`)
+
+		require.ErrorContains(t, err, "parse error near")
+	})
 }
