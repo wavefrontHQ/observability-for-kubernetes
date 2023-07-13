29c13694a24293f7c241195a6fe96977de57a5f0
Yuqi Jin <jyuqi@vmware.com>
2023-07-13 14:08:31 -0700

    initialize work status metric test for jobs
    Co-authored-by: Yuqi Jin <jyuqi@vmware.com>

--- collector/plugins/sources/kstate/workload_test.go
+++ collector/plugins/sources/kstate/workload_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/stretchr/testify/assert"
@@ -13,13 +14,13 @@ import (
 	"github.com/wavefronthq/observability-for-kubernetes/collector/internal/wf"
 )
 
-func setupBasicWorkload() *appsv1.Deployment {
+func setupBasicDeploymentWorkload() *appsv1.Deployment {
 	return &appsv1.Deployment{
 		TypeMeta: metav1.TypeMeta{
 			Kind: "Deployment",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:   "basic-workload",
+			Name:   "basic-deployment-workload",
 			Labels: nil,
 		},
 		Spec: appsv1.DeploymentSpec{},
@@ -27,6 +28,21 @@ func setupBasicWorkload() *appsv1.Deployment {
 			ReadyReplicas:     1,
 			AvailableReplicas: 1,
 		},
+	}
+}
+
+func setupBasicPodWorkload() *corev1.Pod {
+	return &corev1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "Pod",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            "basic-pod-workload",
+			Labels:          nil,
+			OwnerReferences: nil, // no owner
+		},
+		Spec:   corev1.PodSpec{},
+		Status: corev1.PodStatus{},
 	}
 }
 
@@ -38,14 +54,14 @@ func Test_buildWorkloadStatusMetric(t *testing.T) {
 	testTransform := setupWorkloadTransform()
 	timestamp := time.Now().Unix()
 
-	t.Run("test for workload status ready", func(t *testing.T) {
-		testDeployment := setupBasicWorkload()
+	t.Run("test for deployment workload status ready", func(t *testing.T) {
+		testDeployment := setupBasicDeploymentWorkload()
 		numberDesired := 1.0
 		numberReady := 1.0
 
 		testTags := buildWorkloadTags(testDeployment.Kind, testDeployment.Name, "", testTransform.Tags)
 
-		assert.Equal(t, "basic-workload", testTags[workloadNameTag])
+		assert.Equal(t, "basic-deployment-workload", testTags[workloadNameTag])
 		assert.Equal(t, "Deployment", testTags[workloadKindTag])
 
 		actualWFPoint := buildWorkloadStatusMetric(testTransform.Prefix, numberDesired, numberReady, timestamp, testTransform.Source, testTags)
@@ -55,14 +71,14 @@ func Test_buildWorkloadStatusMetric(t *testing.T) {
 		assert.Equal(t, workloadReady, point.Value)
 	})
 
-	t.Run("test for workload status not ready", func(t *testing.T) {
-		testDeployment := setupBasicWorkload()
+	t.Run("test for deployment workload status not ready", func(t *testing.T) {
+		testDeployment := setupBasicDeploymentWorkload()
 		numberDesired := 1.0
 		numberReady := 0.0
 
 		testTags := buildWorkloadTags(testDeployment.Kind, testDeployment.Name, "", testTransform.Tags)
 
-		assert.Equal(t, "basic-workload", testTags[workloadNameTag])
+		assert.Equal(t, "basic-deployment-workload", testTags[workloadNameTag])
 		assert.Equal(t, "Deployment", testTags[workloadKindTag])
 
 		actualWFPoint := buildWorkloadStatusMetric(testTransform.Prefix, numberDesired, numberReady, timestamp, testTransform.Source, testTags)
@@ -71,6 +87,24 @@ func Test_buildWorkloadStatusMetric(t *testing.T) {
 		assert.Equal(t, "kubernetes.workload.status", point.Name())
 		assert.Equal(t, workloadNotReady, point.Value)
 	})
+
+	t.Run("test for pod workload status ready", func(t *testing.T) {
+		testDeployment := setupBasicPodWorkload()
+		numberDesired := 1.0
+		numberReady := 1.0
+
+		testTags := buildWorkloadTags(testDeployment.Kind, testDeployment.Name, "", testTransform.Tags)
+
+		assert.Equal(t, "basic-pod-workload", testTags[workloadNameTag])
+		assert.Equal(t, "Pod", testTags[workloadKindTag])
+
+		actualWFPoint := buildWorkloadStatusMetric(testTransform.Prefix, numberDesired, numberReady, timestamp, testTransform.Source, testTags)
+		point := actualWFPoint.(*wf.Point)
+
+		assert.Equal(t, "kubernetes.workload.status", point.Name())
+		assert.Equal(t, workloadReady, point.Value)
+	})
+
 }
 
 func getTestWFMetricNames(points []wf.Metric) []string {
