34d7f831da8a919e1eda3b9d7278e300e4543f84
Matthew Conger-Eldeen <matthewco@vmware.com>
2023-07-14 09:30:06 -0600

    WIP: get tests working
    Signed-off-by: Mark Michael <mamichael@vmware.com>

--- operator/hack/test/run-e2e-tests.sh
+++ operator/hack/test/run-e2e-tests.sh
@@ -543,7 +543,7 @@ function run_test() {
     run_k8s_events_checks
   fi
 
-  if [[ " ${checks[*]} " =~ " common-metrics " ]]; then
+  if [[ " ${checks[*]} " =~ " common-metrics-check " ]]; then
     run_metrics_checks
   fi
 
@@ -659,7 +659,7 @@ function main() {
     run_test "basic" "health" "static_analysis"
   fi
   if [[ " ${tests_to_run[*]} " =~ " common-metrics " ]]; then
-    run_test "common-metrics"
+    run_test "common-metrics" "common-metrics-check"
   fi
   if [[ " ${tests_to_run[*]} " =~ " advanced " ]]; then
     run_test "advanced" "health" "test_wavefront_metrics" "logging" "proxy"
