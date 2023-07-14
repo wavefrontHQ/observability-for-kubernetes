d5969bcdbb1b485a5cc0017de390a7234682a9d6
Ginwoo Pak <pginwoo@vmware.com>
2023-07-14 13:44:01 -0700

    added helm shim to scripts
    Co-authored-by: Matthew Conger-Eldeen <matthewco@vmware.com>

--- scripts/deploy/uninstall-wavefront-helm-release.sh
+++ scripts/deploy/uninstall-wavefront-helm-release.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-RELEASE=$(helm list -n wavefront | awk '/wavefront/ {print $1}')
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+RELEASE=$("$REPO_ROOT"/bin/helm list -n wavefront | awk '/wavefront/ {print $1}')
 if [ -z ${RELEASE} ]; then exit 0; fi
 
 echo "uninstalling wavefront helm release"
-helm uninstall wavefront --namespace wavefront &>/dev/null || true
+"$REPO_ROOT"/bin/helm uninstall wavefront --namespace wavefront &>/dev/null || true
 kubectl delete namespace wavefront &>/dev/null || true
