b4cf21db952b3c19c0a3dd44360d92f8e1557f87
Matthew Conger-Eldeen <matthewco@vmware.com>
2023-08-08 13:14:40 -0700

    removed echos and added delete_crd and clusterrolebindings
    Co-authored-by: Ginwoo Pak <pginwoo@vmware.com>

--- scripts/clean-cluster.sh
+++ scripts/clean-cluster.sh
@@ -5,11 +5,14 @@ function delete_cluster_roles() {
   local cluster_roles=''
 
   cluster_roles=$(kubectl get clusterroles | awk '/wavefront/ {print $1}')
+  cluster_role_bindings=$(kubectl get clusterrolebindings | awk '/wavefront/ {print $1}')
 
   if [[ -n "${cluster_roles}" ]]; then
-    echo -e "Found ClusterRoles:\n${cluster_roles}"
     kubectl delete --ignore-not-found=true --wait=false clusterroles ${cluster_roles} || true
-    kubectl delete --ignore-not-found=true --wait=false clusterrolebindings ${cluster_roles} || true
+  fi
+
+  if [[ -n "${cluster_role_bindings}" ]]; then
+    kubectl delete --ignore-not-found=true --wait=false clusterrolebindings ${cluster_role_bindings} || true
   fi
 }
 
@@ -19,10 +22,13 @@ function delete_namespaces() {
   ns=$(kubectl get namespaces | awk '/wavefront|collector-targets|observability-system|custom-namespace/ {print $1}')
 
   if [[ -n "${ns}" ]]; then
-    echo -e "Found Namespaces:\n${ns}"
     kubectl patch -n collector-targets pod/pod-stuck-in-terminating -p '{"metadata":{"finalizers":null}}' &>/dev/null || true
-    kubectl delete --ignore-not-found=true --wait=false namespace ${ns} || true
+    kubectl delete --ignore-not-found=true --wait=true namespace ${ns} || true
   fi
+}
+
+function delete_crd() {
+    kubectl delete --ignore-not-found=true --wait=false crd wavefronts.wavefront.com || true
 }
 
 function delete_security_context_constraints() {
@@ -31,7 +37,6 @@ function delete_security_context_constraints() {
   scc=$(kubectl get scc | awk '/wavefront|custom-namespace/ {print $1}')
 
   if [[ -n "${scc}" ]]; then
-    echo -e "Found SecurityContextConstraints:\n${scc}"
     kubectl delete --ignore-not-found=true --wait=true scc ${scc} || true
   fi
 }
@@ -43,6 +48,7 @@ function main() {
 
   delete_cluster_roles
   delete_namespaces
+  delete_crd
 
   if kubectl get scc &>/dev/null; then
     delete_security_context_constraints
