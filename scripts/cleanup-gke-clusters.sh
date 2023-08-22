1feb3a4e2c0def1bec3d4110ba45756a964ed62f
Jerry Belmonte <bjerry@vmware.com>
2023-08-21 19:55:44 -0700

    Remove usage of jq in cleanup script

--- scripts/cleanup-gke-clusters.sh
+++ scripts/cleanup-gke-clusters.sh
@@ -6,15 +6,12 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 function main() {
   cd "${REPO_ROOT}"
 
-  gcloud config set project wavefront-gcp-dev
-
-  CLUSTERS_TO_REMOVE=$(gcloud container clusters list --filter="resourceLabels.delete-me=true" --format json | jq '.[]' | jq -c '{name,zone}')
+  CLUSTERS_TO_REMOVE=$(gcloud container clusters list --project wavefront-gcp-dev --filter="resourceLabels.delete-me=true" --format="csv[no-heading](name,zone)")
 
   local name zone
   for cluster in ${CLUSTERS_TO_REMOVE[@]}; do
-    name=$(echo ${cluster} | jq -r '.name')
-    zone=$(echo ${cluster} | jq -r '.zone')
-    zone=${zone/us-central1-/}
+    name=$(echo ${cluster} | cut -d ',' -f 1)
+    zone=$(echo ${cluster} | cut -d ',' -f 2 | cut -d '-' -f 3)
     GKE_CLUSTER_NAME=${name} GCP_ZONE=${zone} make delete-gke-cluster || true
   done
 }
