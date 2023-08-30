759c717b1ef2293771587a360e1e964e352bc650
John Cornish <jcornish@vmware.com>
2023-08-30 16:25:25 -0600

    hopefully add kind binary install

--- collector/hack/jenkins/setup-for-integration-test.sh
+++ collector/hack/jenkins/setup-for-integration-test.sh
@@ -44,6 +44,17 @@ if [[ "${K8S_ENV}" == "eks" ]]; then
   fi
 fi
 
+if [[ "${K8S_ENV}" == "kind" ]]; then
+  if ! [ -x "$(command -v kind)" ]; then
+    # For AMD64 / x86_64
+    [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+    # For ARM64
+    [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-arm64
+    chmod +x ./kind
+    sudo mv ./kind /usr/local/bin/kind
+  fi
+fi
+
 #
 # jq
 #
