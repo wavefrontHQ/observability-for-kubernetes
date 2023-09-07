63ad2b8ebf367a7f7a1af05d0b138f1ac6f1d17a
Mark Michael <mamichael@vmware.com>
2023-09-07 10:05:35 -0600

    try finding clusters that have expired

--- scripts/cleanup-gke-clusters.sh
+++ scripts/cleanup-gke-clusters.sh
@@ -7,13 +7,27 @@ function main() {
   cd "${REPO_ROOT}"
   gcloud auth activate-service-account --key-file "$GCP_CREDS"
   gcloud config set project wavefront-gcp-dev
-  CLUSTERS_TO_REMOVE=$(gcloud container clusters list --project wavefront-gcp-dev --filter="resourceLabels.delete-me=true" --format="csv[no-heading](name,zone)")
+  CLUSTERS_TO_REMOVE=$(gcloud container clusters list \
+    --project wavefront-gcp-dev \
+    --filter="resourceLabels.delete-me=true" \
+    --format="csv[no-heading](name,zone,resourceLabels)")
 
-  local name zone
+  echo "CLUSTERS_TO_REMOVE: ${CLUSTERS_TO_REMOVE}"
+
+  local name zone expires_in_days expired_creation_time
   for cluster in ${CLUSTERS_TO_REMOVE[@]}; do
     name=$(echo ${cluster} | cut -d ',' -f 1)
     zone=$(echo ${cluster} | cut -d ',' -f 2 | cut -d '-' -f 3)
-    GKE_CLUSTER_NAME=${name} GCP_ZONE=${zone} make delete-gke-cluster &
+    expires_in_days=$(echo ${cluster} | cut -d ',' -f 3 | sed 's/.*expires-in-days=//g')
+    echo "extracting name '${name}', zone '${zone}', and expires-in-days '${expires_in_days}' from '${cluster}'"
+
+    expired_creation_time=$(date --date="${expires_in_days} day ago" +%y%m%d)
+    echo "now vs expired_creation_time: $(date +%s) vs ${expired_creation_time}"
+    gcloud container clusters list \
+        --project wavefront-gcp-dev \
+        --filter="name=${name},creationTimestamp.date(\"+%s\")<${expired_creation_time}" \
+        --format="csv[no-heading](name,zone)"
+#    GKE_CLUSTER_NAME=${name} GCP_ZONE=${zone} GKE_WAIT_FOR_COMPLETE=false make delete-gke-cluster &
   done
 }
 
