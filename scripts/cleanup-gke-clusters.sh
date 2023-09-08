d6cda9e69bc90141d5c3afcb00ee53d8b5810917
Jerry Belmonte <bjerry@vmware.com>
2023-09-08 12:06:56 -0600

    hacky filter for cluster and delete
    Signed-off-by: John Cornish <jcornish@vmware.com>

--- scripts/cleanup-gke-clusters.sh
+++ scripts/cleanup-gke-clusters.sh
@@ -12,22 +12,28 @@ function main() {
     --filter="resourceLabels.delete-me=true" \
     --format="csv[no-heading](name,zone,resourceLabels)")
 
-  echo "CLUSTERS_TO_REMOVE: ${CLUSTERS_TO_REMOVE}"
-
   local name zone expires_in_days expired_creation_time
   for cluster in ${CLUSTERS_TO_REMOVE[@]}; do
     name=$(echo ${cluster} | cut -d ',' -f 1)
     zone=$(echo ${cluster} | cut -d ',' -f 2 | cut -d '-' -f 3)
     expires_in_days=$(echo ${cluster} | cut -d ',' -f 3 | sed 's/.*expires-in-days=//g')
-    echo "extracting name '${name}', zone '${zone}', and expires-in-days '${expires_in_days}' from '${cluster}'"
 
-    expired_creation_time=$(date --date="${expires_in_days} day ago" +%y%m%d)
+#    echo "extracting name '${name}', zone '${zone}', and expires-in-days '${expires_in_days}' from '${cluster}'"
+
+    expired_creation_time=$(date --date="${expires_in_days} day ago" +%s)
     echo "now vs expired_creation_time: $(date +%s) vs ${expired_creation_time}"
-    gcloud container clusters list \
+    echo "### LISTING MATCHING CLUSTERS ###"
+    cluster=$(gcloud container clusters list \
         --project wavefront-gcp-dev \
-        --filter="name=${name},creationTimestamp.date(\"+%s\")<${expired_creation_time}" \
-        --format="csv[no-heading](name,zone)"
-#    GKE_CLUSTER_NAME=${name} GCP_ZONE=${zone} GKE_WAIT_FOR_COMPLETE=false make delete-gke-cluster &
+        --filter="createTime.date(\"+%s\")>${expired_creation_time}" \
+        --format="csv[no-heading](name,zone)" \
+        | grep "${name}")
+    if [ -z "${cluster}" ]; then
+        continue
+    fi
+    name=$(echo ${cluster} | cut -d ',' -f 1)
+    zone=$(echo ${cluster} | cut -d ',' -f 2 | cut -d '-' -f 3)
+    echo GKE_CLUSTER_NAME=${name} GCP_ZONE=${zone} GKE_WAIT_FOR_COMPLETE=false make delete-gke-cluster
   done
 }
 
