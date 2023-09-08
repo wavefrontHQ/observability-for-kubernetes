d9c112bde5b4ee27dc37ac97b5108a31339843f4
John Cornish <jcornish@vmware.com>
2023-09-08 12:38:03 -0600

    Cleanup and actually delete
    Signed-off-by: Jerry Belmonte <bjerry@vmware.com>

--- scripts/cleanup-gke-clusters.sh
+++ scripts/cleanup-gke-clusters.sh
@@ -7,25 +7,29 @@ function main() {
   cd "${REPO_ROOT}"
   gcloud auth activate-service-account --key-file "$GCP_CREDS"
   gcloud config set project wavefront-gcp-dev
-  CLUSTERS_TO_REMOVE=$(gcloud container clusters list \
+  CLUSTERS_TO_REMOVE=$(\
+    gcloud container clusters list \
     --project wavefront-gcp-dev \
     --filter="resourceLabels.delete-me=true" \
-    --format="csv[no-heading](name,zone,resourceLabels)")
+    --format="csv[no-heading](name,zone,resourceLabels)"\
+  )
 
-  local name zone expires_in_days expired_creation_time
+  local name zone expires_in_days expired_time
   for cluster in ${CLUSTERS_TO_REMOVE[@]}; do
     name=$(echo ${cluster} | cut -d ',' -f 1)
     zone=$(echo ${cluster} | cut -d ',' -f 2 | cut -d '-' -f 3)
 
     if echo "${cluster}" | grep 'expires-in-days' >/dev/null; then
-      expires_in_days=$(echo ${cluster} | cut -d ',' -f 3 | sed 's/.*expires-in-days=//g')
+      expires_in_days=$(\
+        echo ${cluster} \
+        | cut -d ',' -f 3 \
+        | sed 's/.*expires-in-days=//g'\
+      )
 
-      expired_creation_time=$(date --date="${expires_in_days} day ago" +%s)
-      echo "now vs expired_creation_time: $(date +%s) vs ${expired_creation_time}"
-      echo "### LISTING MATCHING CLUSTERS ###"
+      expired_time=$(date --date="${expires_in_days} day ago" +%s)
       cluster=$(gcloud container clusters list \
         --project wavefront-gcp-dev \
-        --filter="createTime.date(\"+%s\")<${expired_creation_time}" \
+        --filter="createTime.date(\"+%s\")<${expired_time}" \
         --format="csv[no-heading](name,zone)" \
         | grep "${name}" || echo "")
       if [ -z "${cluster}" ]; then
@@ -33,7 +37,7 @@ function main() {
       fi
     fi
 
-    echo GKE_CLUSTER_NAME=${name} GCP_ZONE=${zone} GKE_WAIT_FOR_COMPLETE=false make delete-gke-cluster
+    GKE_CLUSTER_NAME=${name} GCP_ZONE=${zone} GKE_WAIT_FOR_COMPLETE=false make delete-gke-cluster
   done
 }
 
