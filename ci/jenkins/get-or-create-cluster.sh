8eed7095f1abc45abbea07959e082fd9f26dd158
John Cornish <jcornish@vmware.com>
2023-09-18 12:04:13 -0600

    Explicitly set expiration to 10h just in case

--- ci/jenkins/get-or-create-cluster.sh
+++ ci/jenkins/get-or-create-cluster.sh
@@ -10,7 +10,7 @@ make -C "${REPO_ROOT}" gke-cluster-name-check
 # Assume it's because it doesn't exist and create it
 if ! make -C "${REPO_ROOT}" gke-connect-to-cluster; then
     echo "Did not find cluster '${GKE_CLUSTER_NAME}', creating ..."
-    make -C "${REPO_ROOT}" create-gke-cluster
+    make -C "${REPO_ROOT}" create-gke-cluster GKE_EXPIRES_IN_HOURS=10
     exit 0
 fi
 
