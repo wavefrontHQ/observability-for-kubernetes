142e9bc7f490982a0ccb0881f50028a870397a04
Kyle Tolliver <ktolliver@vmware.com>
2023-09-18 16:27:57 -0600

    Add script to push metric to le mans
    Signed-off-by: Mark Douglass <mdouglass@vmware.com>

---
+++ le-mans/scripts/post-le-mans-metric.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+STREAM_NAME=experiment7
+LEMANS_GATEWAY_SERVER="localhost:8002"
+CSP_SECRET="$(echo -n "$(cat tmp/csp_username):$(cat tmp/csp_password)" | base64)"
+
+token_file=$(mktemp)
+
+echo "authenticating"
+curl --fail-with-body --location --request POST 'https://console-stg.cloud.vmware.com/csp/gateway/am/api/auth/authorize' \
+  --header 'Content-Type: application/x-www-form-urlencoded' \
+  --header "Authorization: Basic $CSP_SECRET" \
+  --data-urlencode 'grant_type=client_credentials' \
+  -o "$token_file"
+
+CSP_AUTH_TOKEN="$(gojq -r .access_token  "$token_file")"
+
+
+curl --location --fail-with-body --request POST "http://$LEMANS_GATEWAY_SERVER/le-mans/v1/streams/$STREAM_NAME" \
+  --header "x-xenon-auth-token: #CSP#$CSP_AUTH_TOKEN" \
+  --header 'Content-Type: text/plain' \
+  --data "test.luke-to-lemans-via-troll 100 source=luke-dev-mac"
