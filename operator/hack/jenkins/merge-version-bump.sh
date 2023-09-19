913e9ec711599a717da0a6e40d21e7633a7db170
Jerry Belmonte <bjerry@vmware.com>
2023-09-18 21:23:27 -0700

    Use latest GitHub Api Version for merge version bump script

--- operator/hack/jenkins/merge-version-bump.sh
+++ operator/hack/jenkins/merge-version-bump.sh
@@ -5,7 +5,7 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 source "${REPO_ROOT}/scripts/k8s-utils.sh"
 cd "$REPO_ROOT"
 
-VERSION=$(cat ${REPO_ROOT}/operator/release/OPERATOR_VERSION)
+VERSION=$(cat "${REPO_ROOT}/operator/release/OPERATOR_VERSION")
 GIT_BUMP_BRANCH_NAME="release-operator-${VERSION}"
 git branch -D "$GIT_BUMP_BRANCH_NAME" &>/dev/null || true
 git checkout -b "$GIT_BUMP_BRANCH_NAME"
@@ -17,19 +17,21 @@ git add deploy/ docs/ README.md operator/config/manager/kustomization.yaml
 git commit -am "Release operator version: ${VERSION}"
 git push --force --set-upstream origin "${GIT_BUMP_BRANCH_NAME}"
 
-
-PR_URL=$(curl \
+PR_URL=$(curl -sSL \
   -X POST \
-  -H "Authorization: token ${GITHUB_TOKEN}" \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
   -d "{\"head\":\"${GIT_BUMP_BRANCH_NAME}\",\"base\":\"main\",\"title\":\"Release operator version: ${VERSION}\"}" \
   https://api.github.com/repos/wavefrontHQ/observability-for-kubernetes/pulls |
   jq -r '.url')
 
 echo "PR URL: ${PR_URL}"
 
-curl \
+curl -sSL \
   -X PUT \
-  -H "Authorization: token ${GITHUB_TOKEN}" \
-  -H "Accept: application/vnd.github.v3+json" \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
   "${PR_URL}/merge" \
   -d "{\"commit_title\":\"Release operator version: ${VERSION}\", \"merge_method\":\"squash\"}"
