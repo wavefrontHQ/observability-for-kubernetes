b8cabf0769e90e878a41176c1e6befc9a67d422a
Ginwoo Pak <pginwoo@vmware.com>
2023-09-19 13:56:29 -0700

    WIP: test logging resources in separate component

--- operator/components/logging_test.go
+++ operator/components/logging_test.go
@@ -4,6 +4,11 @@ import (
 	"os"
 	"testing"
 
+	"github.com/wavefronthq/observability-for-kubernetes/operator/internal/util"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	"github.com/stretchr/testify/require"
 	"github.com/wavefronthq/observability-for-kubernetes/operator/internal/testhelper/wftest"
 )
@@ -34,9 +39,26 @@ func TestProcessAndValidate(t *testing.T) {
 func TestResources(t *testing.T) {
 	t.Run("default configuration", func(t *testing.T) {
 		loggingComponent := NewLoggingComponent(validLoggingComponentConfig(), os.DirFS(DeployDir))
+		result := loggingComponent.PreprocessAndValidate()
+		require.True(t, result.IsValid())
+
 		toApply, toDelete, err := loggingComponent.Resources()
 		require.Nil(t, err)
 		require.NotEmpty(t, toApply)
+		ds, err := GetAppliedDaemonSet("logging", toApply)
+		require.Nil(t, err)
+		require.Equal(t, loggingComponent.Config.ConfigHash, ds.Spec.Template.GetObjectMeta().GetAnnotations()["configHash"])
+		require.Equal(t, ds.Name, util.LoggingName)
+		require.Equal(t, ds.Spec.Template.GetLabels()["name"], util.LoggingName)
+		require.Equal(t, ds.Spec.Template.GetLabels()["app.kubernetes.io/name"], "wavefront")
+		require.Equal(t, ds.Spec.Template.GetLabels()["app.kubernetes.io/component"], "logging")
+
+		require.Equal(t, ds.Namespace, wftest.DefaultNamespace)
+		require.Equal(t, ds.Spec.Template.GetAnnotations()["proxy-available-replicas"], "1")
+		require.NotEmpty(t, ds.Spec.Template.GetObjectMeta().GetAnnotations()["configHash"])
+		require.Equal(t, ds.Spec.Template.Spec.Containers[0].Image, wftest.DefaultImageRegistry+"/kubernetes-operator-fluentbit:"+loggingComponent.Config.LoggingVersion)
+		require.Equal(t, ds.Spec.Template.Spec.Containers[0].Env[1].Value, loggingComponent.Config.ClusterName)
+
 		require.Empty(t, toDelete)
 	})
 
@@ -44,10 +66,31 @@ func TestResources(t *testing.T) {
 
 func validLoggingComponentConfig() LoggingComponentConfig {
 	return LoggingComponentConfig{
-		ClusterName:    wftest.DefaultClusterName,
-		Namespace:      wftest.DefaultNamespace,
-		LoggingVersion: "2.1.2",
-		ImageRegistry:  wftest.DefaultImageRegistry,
-		ProxyAddress:   wftest.DefaultProxyAddress,
+		ClusterName:            wftest.DefaultClusterName,
+		Namespace:              wftest.DefaultNamespace,
+		LoggingVersion:         "2.1.2",
+		ImageRegistry:          wftest.DefaultImageRegistry,
+		ProxyAddress:           wftest.DefaultProxyAddress,
+		ProxyAvailableReplicas: 1,
 	}
+}
+
+func GetAppliedDaemonSet(metadataName string, toApply []client.Object) (appsv1.DaemonSet, error) {
+
+	var found client.Object
+	for _, clientObject := range toApply {
+		if clientObject.GetObjectKind().GroupVersionKind().Kind == "DaemonSet" {
+			found = clientObject
+		}
+	}
+
+	var daemonSet appsv1.DaemonSet
+	unstructuredObj, _ := runtime.DefaultUnstructuredConverter.ToUnstructured(found)
+
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstructuredObj, &daemonSet)
+	if err != nil {
+		return appsv1.DaemonSet{}, err
+	}
+
+	return daemonSet, nil
 }
