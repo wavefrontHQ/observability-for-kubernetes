b10c5134d547cecc0078844f8064bc60b1a08a52
Anil Kodali <akodali@vmware.com>
2023-09-19 15:41:40 -0700

    refactor tests

--- operator/hack/test/advanced-test.sh
+++ operator/hack/test/advanced-test.sh
@@ -1,1 +1,18 @@
-exit_on_fail wait_for_query_match_exact "at(%22end%22%2C%202m%2C%20ts(kubernetes.collector.version%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22%20AND%20installation_method%3D%22operator%22%20AND%20processed%3D%22true%22))" "${COLLECTOR_VERSION_IN_DECIMAL}"
+OPERATOR_NAMESPACE="observability-system"
+PROXY_POD_NAME="wavefront-proxy*"
+PROXY_CONTAINER_NAME="wavefront-proxy"
+
+exit_on_fail wait_for_query_match_exact "at(\"end\", 2m, ts(kubernetes.collector.version, cluster=\"${CONFIG_CLUSTER_NAME}\" and installation_method=\"operator\" and processed=\"true\"))" "${COLLECTOR_VERSION_IN_DECIMAL}"
+
+exit_on_fail \
+  wait_for_query_match_exact \
+  "ts(kubernetes.controlplane.etcd.server.has.leader.gauge, cluster=\"${CONFIG_CLUSTER_NAME}\")" \
+  "1"
+
+exit_on_fail \
+  wait_for_query_non_zero \
+  "ts(kubernetes.controlplane.etcd.network.client.grpc.received.bytes.total.counter, cluster=\"${CONFIG_CLUSTER_NAME}\")"
+
+exit_on_fail \
+  wait_for_query_non_zero \
+  "at(\"end\", 2m, ts(kubernetes.cadvisor.container.cpu.cfs.throttled.seconds.total.counter, cluster=\"${K8S_CLUSTER_NAME}\" and namespace_name=\"${OPERATOR_NAMESPACE}\" and pod_name=\"${PROXY_POD_NAME}\" and container_name=\"${PROXY_CONTAINER_NAME}\"))"
