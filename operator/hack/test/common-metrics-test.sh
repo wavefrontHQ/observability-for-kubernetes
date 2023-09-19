24f602122f7a828cf9f85e752b00932278ee9b3b
Anil Kodali <akodali@vmware.com>
2023-09-19 14:27:10 -0700

    Add e2e test

--- operator/hack/test/common-metrics-test.sh
+++ operator/hack/test/common-metrics-test.sh
@@ -1,3 +1,7 @@
+
+OPERATOR_NAMESPACE="observability-system"
+PROXY_POD_NAME="wavefront-proxy*"
+PROXY_CONTAINER_NAME="wavefront-proxy"
 exit_on_fail \
   wait_for_query_match_exact \
   "ts(kubernetes.controlplane.etcd.server.has.leader.gauge%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22)" \
@@ -6,3 +10,7 @@ exit_on_fail \
 exit_on_fail \
   wait_for_query_non_zero \
   "ts(kubernetes.controlplane.etcd.network.client.grpc.received.bytes.total.counter%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22)"
+
+exit_on_fail \
+  wait_for_query_non_zero \
+  "at(\"end\", 2m, ts(kubernetes.cadvisor.container.cpu.cfs.throttled.seconds.total.counter, cluster=\"${K8S_CLUSTER_NAME}\" and namespace_name=\"${OPERATOR_NAMESPACE}\" and pod_name=\"${PROXY_POD_NAME}\" and container_name=\"${PROXY_CONTAINER_NAME}\"))"
