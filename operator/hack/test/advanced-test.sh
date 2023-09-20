5820a823d4af701f917b5f9d44321cea742552a9
Anil Kodali <akodali@vmware.com>
2023-09-20 09:08:30 -0700

    remove control plane metrics

--- operator/hack/test/advanced-test.sh
+++ operator/hack/test/advanced-test.sh
@@ -4,14 +4,5 @@ PROXY_CONTAINER_NAME="wavefront-proxy"
 exit_on_fail wait_for_query_match_exact "at(%22end%22%2C%202m%2C%20ts(kubernetes.collector.version%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22%20AND%20installation_method%3D%22operator%22%20AND%20processed%3D%22true%22))" "${COLLECTOR_VERSION_IN_DECIMAL}"
 
 exit_on_fail \
-  wait_for_query_match_exact \
-  "ts(kubernetes.controlplane.etcd.server.has.leader.gauge%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22)" \
-  "1"
-
-exit_on_fail \
-  wait_for_query_non_zero \
-  "ts(kubernetes.controlplane.etcd.network.client.grpc.received.bytes.total.counter%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22)"
-
-exit_on_fail \
   wait_for_query_non_zero \
   "at(%22end%22%2C%202m%2C%20ts(kubernetes.cadvisor.container.cpu.cfs.throttled.seconds.total.counter%2C%20cluster%3D%22${K8S_CLUSTER_NAME}%22%20AND%20namespace_name%3D%22${OPERATOR_NAMESPACE}%22%20AND%20pod_name%3D%22${PROXY_POD_NAME}%22%20AND%20container_name%3D%22${PROXY_CONTAINER_NAME}%22))"
