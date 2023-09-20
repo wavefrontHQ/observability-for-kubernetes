ad4174a6a837c4b5e21980a4da672d9e62b2fd33
Yuqi Jin <jyuqi@vmware.com>
2023-09-20 15:30:55 -0700

    Create one link for all alerts instead of individual alert links
    Co-authored-by: Priya Selvaganesan <pselvaganesa@vmware.com>

--- operator/dev-internal/docs/alerts/create-all-alerts.sh
+++ operator/dev-internal/docs/alerts/create-all-alerts.sh
@@ -52,7 +52,8 @@ function create_alerts() {
     done
   popd >/dev/null
 
-  echo "Finished creating alerts."
+  echo "Link to alerts just created: https://${wavefront_cluster}.wavefront.com/alerts?search=%7B%22searchTerms%22%3A%5B%7B%22type%22%3A%22tagpath%22%2C%22value%22%3A%22integration.kubernetes%22%2C%22negated%22%3Afalse%7D%2C%7B%22type%22%3A%22freetext%22%2C%22value%22%3A%22${k8s_cluster_name}%22%2C%22negated%22%3Afalse%7D%5D%2C%22sortOrder%22%3A%22ascending%22%2C%22sortField%22%3Anull%2C%22pageNum%22%3A1%7D&tagPathTree=%7B%22integration%22%3A%7B%22wf-value%22%3A%22integration%22%7D%7D"
+
 }
 
 function post_alert_to_wavefront() {
@@ -84,7 +85,7 @@ function post_alert_to_wavefront() {
   local alert_id
   alert_id=$(sed -n 's/.*id":"\([0-9]*\).*/\1/p' "${response}")
 
-  echo "Alert has been created at: https://${wavefront_cluster}.wavefront.com/alerts/${alert_id}"
+  echo "Alert has been created."
 }
 
 function check_alert_file() {
