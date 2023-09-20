252278ec8ef3218c81ac23162ece444a8f3c6185
John Cornish <jcornish@vmware.com>
2023-09-20 11:13:46 -0600

    Different var name in operator vs. collector e2e test

--- operator/hack/test/advanced-test.sh
+++ operator/hack/test/advanced-test.sh
@@ -5,4 +5,4 @@ exit_on_fail wait_for_query_match_exact "at(%22end%22%2C%202m%2C%20ts(kubernetes
 
 exit_on_fail \
   wait_for_query_non_zero \
-  "at(%22end%22%2C%202m%2C%20ts(kubernetes.cadvisor.container.cpu.cfs.throttled.seconds.total.counter%2C%20cluster%3D%22${K8S_CLUSTER_NAME}%22%20AND%20namespace_name%3D%22${OPERATOR_NAMESPACE}%22%20AND%20pod_name%3D%22${PROXY_POD_NAME}%22%20AND%20container_name%3D%22${PROXY_CONTAINER_NAME}%22))"
+  "at(%22end%22%2C%202m%2C%20ts(kubernetes.cadvisor.container.cpu.cfs.throttled.seconds.total.counter%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22%20AND%20namespace_name%3D%22${OPERATOR_NAMESPACE}%22%20AND%20pod_name%3D%22${PROXY_POD_NAME}%22%20AND%20container_name%3D%22${PROXY_CONTAINER_NAME}%22))"
