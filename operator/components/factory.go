cf305fe82b7e4e5e40f628054109e47b5352f29c
mamichael <mamichael@vmware.com>
2023-09-20 13:03:20 -0600

    WIP: creates factory for building components and resources

---
+++ operator/components/factory.go
@@ -0,0 +1,36 @@
+package components
+
+import (
+	"os"
+
+	wf "github.com/wavefronthq/observability-for-kubernetes/operator/api/v1alpha1"
+)
+
+func BuildComponents(wf *wf.Wavefront) (components map[Component]bool, error error) {
+	createdComponents := make(map[Component]bool)
+	config := LoggingComponentConfig{
+		ClusterName:     wf.Spec.ClusterName,
+		Namespace:       wf.Spec.Namespace,
+		LoggingVersion:  wf.Spec.DataCollection.Logging.LoggingVersion,
+		ImageRegistry:   wf.Spec.ImageRegistry,
+		ImagePullSecret: wf.Spec.ImagePullSecret,
+
+		ProxyAddress:           wf.Spec.DataCollection.Logging.ProxyAddress,
+		ProxyAvailableReplicas: wf.Spec.DataExport.WavefrontProxy.AvailableReplicas,
+		Tolerations:            wf.Spec.DataCollection.Tolerations,
+		Resources:              wf.Spec.DataCollection.Logging.Resources,
+		TagAllowList:           wf.Spec.DataCollection.Logging.Filters.TagAllowList,
+		TagDenyList:            wf.Spec.DataCollection.Logging.Filters.TagDenyList,
+		Tags:                   wf.Spec.DataCollection.Logging.Tags,
+
+		ControllerManagerUID: wf.Spec.ControllerManagerUID,
+	}
+
+	loggingComponent, err := NewLoggingComponent(config, os.DirFS(DeployDir))
+	if err != nil {
+		return nil, err
+	}
+
+	createdComponents[&loggingComponent] = wf.Spec.CanExportData && wf.Spec.DataCollection.Logging.Enable
+	return createdComponents, error
+}
