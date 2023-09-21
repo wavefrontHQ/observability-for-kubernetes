f4e0b9c36b975b1ef73bad4d5ed2d23c1058a133
Priya Selvaganesan <1836417+priyaselvaganesan@users.noreply.github.com>
2023-09-21 14:19:46 -0700

    Script to create all alerts from available templates (#375)
    Co-authored-by: Jerry Belmonte <bjerry@vmware.com>
    Co-authored-by: Yuqi Jin <jyuqi@vmware.com>
    Co-authored-by: Jesse Pye <jpye@vmware.com>
    Co-authored-by: Peter Stone <stonepe@vmware.com>

--- operator/dev-internal/docs/alerts/create-alert.sh
+++ operator/dev-internal/docs/alerts/create-alert.sh
@@ -1,11 +1,52 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+function download_alert() {
+  local github_repo="$1"
+  local alert_path="$2"
+  local git_branch="$3"
+  local alert_file="$4"
+  local response res_code
+
+  printf "Downloading alert ..."
+
+  response=$(mktemp)
+  res_code=$(curl --silent --show-error --output "${response}" --write-out "%{http_code}" \
+    "https://api.github.com/repos/${github_repo}/contents/${alert_path}?ref=${git_branch}" \
+    -H "Accept: application/vnd.github+json")
+
+  if [[ ${res_code} -ne 200 ]]; then
+    echo "Unable to download alert: $(cat "${response}")"
+    exit 1
+  fi
+
+  local download_url
+  if [ -x "$(command -v jq)" ]; then
+    download_url=$(jq -r '.download_url' "${response}")
+  else
+    download_url=$(grep download_url "${response}" | tr '",' ' ' | awk '{print $3}')
+  fi
+
+  res_code=$(curl --silent --show-error --output "${alert_file}" --write-out "%{http_code}" -L "${download_url}")
+  if [[ ${res_code} -ne 200 ]]; then
+    echo "Unable to download alert: $(cat "${alert_file}")"
+    exit 1
+  fi
+
+  echo " done."
+}
+
 function post_alert_to_wavefront() {
   local wavefront_token=$1
   local wavefront_cluster=$2
   local alert_file=$3
   local k8s_cluster_name=$4
+  local alert_name response res_code
+
+  if [ -x "$(command -v jq)" ]; then
+    alert_name=$(jq -r '.name' "${alert_file}")
+    echo "Creating alert: ${alert_name}"
+  fi
 
   response=$(mktemp)
   res_code=$(curl --silent --show-error --output "${response}" --write-out "%{http_code}" \
@@ -16,16 +57,11 @@ function post_alert_to_wavefront() {
     -d @<(sed "s/K8S_CLUSTER_NAME/${k8s_cluster_name}/g" "${alert_file}"))
 
   if [[ ${res_code} -ne 200 ]]; then
-    echo "Unable to create alert: "
-    cat "${response}"
+    echo "Unable to create alert: $(cat "${response}")"
     exit 1
   fi
 
-  if [ -x "$(command -v jq)" ]; then
-    alert_name=$(jq -r '.name' "${alert_file}")
-    echo "Alert name: ${alert_name}"
-  fi
-
+  local alert_id
   alert_id=$(sed -n 's/.*id":"\([0-9]*\).*/\1/p' "${response}")
 
   echo "Alert has been created at: https://${wavefront_cluster}.wavefront.com/alerts/${alert_id}"
@@ -68,10 +104,10 @@ function print_usage_and_exit() {
 }
 
 function print_usage() {
-  echo "Usage: create-alert.sh -t <WAVEFRONT_TOKEN> -c <WF_CLUSTER> -f <ALERT_FILE> -n <K8S_CLUSTER_NAME> -h"
+  echo "Usage: create-alert.sh -t <WAVEFRONT_TOKEN> -c <WF_CLUSTER> -f <ALERT_FILE_NAME> -n <K8S_CLUSTER_NAME> -h"
   echo -e "\t-t wavefront token (required)"
   echo -e "\t-c wavefront instance name (required)"
-  echo -e "\t-f path to alert file (required)"
+  echo -e "\t-f alert file name (required)"
   echo -e "\t-n kubernetes cluster name (required)"
   echo -e "\t-h print usage"
 }
@@ -82,12 +118,19 @@ function main() {
   local ALERT_FILE=
   local K8S_CLUSTER_NAME=
 
-  while getopts 'c:t:f:n:h' opt; do
+  # Default arguments
+  local GITHUB_REPO='wavefrontHQ/observability-for-kubernetes'
+  local ALERTS_FOLDER='docs/alerts/templates'
+  local GIT_BRANCH='main'
+
+  while getopts 'c:t:f:n:p:b:h' opt; do
     case "${opt}" in
     t) WAVEFRONT_TOKEN="${OPTARG}" ;;
     c) WF_CLUSTER="${OPTARG}" ;;
     f) ALERT_FILE="${OPTARG}" ;;
     n) K8S_CLUSTER_NAME="${OPTARG}" ;;
+    p) ALERTS_FOLDER="${OPTARG}" ;;
+    b) GIT_BRANCH="${OPTARG}" ;;
     h) print_usage; exit 0 ;;
     \?) print_usage_and_exit "Invalid option" ;;
     esac
@@ -99,8 +142,12 @@ function main() {
   check_required_argument "${ALERT_FILE}" "-f <ALERT_FILE> is required"
   check_required_argument "${K8S_CLUSTER_NAME}" "-n <K8S_CLUSTER_NAME> is required"
 
-  check_alert_file "${ALERT_FILE}"
-  post_alert_to_wavefront "${WAVEFRONT_TOKEN}" "${WF_CLUSTER}" "${ALERT_FILE}" "${K8S_CLUSTER_NAME}"
+  # Download and create the alert
+  TEMP_FILE=$(mktemp)
+  download_alert "${GITHUB_REPO}" "${ALERTS_FOLDER}/${ALERT_FILE}" "${GIT_BRANCH}" "${TEMP_FILE}"
+  check_alert_file "${TEMP_FILE}"
+  post_alert_to_wavefront "${WAVEFRONT_TOKEN}" "${WF_CLUSTER}" "${TEMP_FILE}" "${K8S_CLUSTER_NAME}"
+  rm "${TEMP_FILE}"
 }
 
 main "$@"
