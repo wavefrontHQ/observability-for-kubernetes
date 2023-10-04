c67d131bb78cff9bad59d1b588cc7ddb8c00d9e1
svc.wf-jenkins <svc.wf-jenkins@vmware.com>
2023-10-04 14:50:50 -0700

    Release operator version: 2.14.1

--- docs/alerts/create-all-alerts.sh
+++ docs/alerts/create-all-alerts.sh
@@ -1,44 +1,67 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-function download_alert() {
+function download_alerts() {
   local github_repo="$1"
-  local alert_path="$2"
+  local alerts_path="$2"
   local git_branch="$3"
-  local alert_file="$4"
   local response res_code
 
-  printf "Downloading alert ..."
+  printf "Downloading alerts ..."
 
   response=$(mktemp)
   res_code=$(curl --silent --show-error --output "${response}" --write-out "%{http_code}" \
-    "https://api.github.com/repos/${github_repo}/contents/${alert_path}?ref=${git_branch}" \
+    "https://api.github.com/repos/${github_repo}/contents/${alerts_path}?ref=${git_branch}" \
     -H "Accept: application/vnd.github+json")
 
   if [[ ${res_code} -ne 200 ]]; then
-    print_err_and_exit "Unable to download alert: $(cat "${response}")"
+    print_err_and_exit "Unable to download alerts: $(cat "${response}")"
   fi
 
-  local download_url
-  if [ -x "$(command -v jq)" ]; then
-    download_url=$(jq -r '.download_url' "${response}")
-  else
-    download_url=$(grep download_url "${response}" | tr '",' ' ' | awk '{print $3}')
-  fi
-
-  res_code=$(curl --silent --show-error --output "${alert_file}" --write-out "%{http_code}" -L "${download_url}")
-  if [[ ${res_code} -ne 200 ]]; then
-    print_err_and_exit "Unable to download alert: $(cat "${alert_file}")"
-  fi
+  pushd "${TEMP_DIR}" >/dev/null
+    local download_urls
+    if [ -x "$(command -v jq)" ]; then
+      download_urls=$(jq -r '.[].download_url' "${response}")
+    else
+      download_urls=$(grep download_url "${response}" | awk '{print $2}' | tr '",' ' ')
+    fi
+    # shellcheck disable=SC2068
+    for download_url in ${download_urls[@]}; do
+      res_code=$(curl --silent --show-error --write-out "%{http_code}" -LO "${download_url}")
+      if [[ ${res_code} -ne 200 ]]; then
+        print_err_and_exit "Unable to download alert at: ${download_url}"
+      fi
+    done
+  popd >/dev/null
 
   echo " done."
+}
+
+function create_alerts() {
+  local wavefront_token="$1"
+  local wavefront_cluster="$2"
+  local k8s_cluster_name="$3"
+  local alert_target="$4"
+  local alert_files
+
+  pushd "${TEMP_DIR}" >/dev/null
+    alert_files=$(ls "${TEMP_DIR}")
+    # shellcheck disable=SC2068
+    for alert_file in ${alert_files[@]}; do
+      check_alert_file "${alert_file}"
+      post_alert_to_wavefront "${WAVEFRONT_TOKEN}" "${WF_CLUSTER}" "${K8S_CLUSTER_NAME}" "${alert_file}" "${alert_target}"
+    done
+  popd >/dev/null
+
+  echo "Link to alerts just created: https://${wavefront_cluster}.wavefront.com/alerts?search=%7B%22searchTerms%22%3A%5B%7B%22type%22%3A%22tagpath%22%2C%22value%22%3A%22integration.kubernetes%22%7D%2C%7B%22type%22%3A%22freetext%22%2C%22value%22%3A%22${k8s_cluster_name}%22%7D%5D%2C%22sortOrder%22%3A%22ascending%22%2C%22sortField%22%3Anull%2C%22pageNum%22%3A1%7D&tagPathTree=%7B%22integration%22%3A%7B%22wf-value%22%3A%22integration%22%7D%7D"
+
 }
 
 function post_alert_to_wavefront() {
   local wavefront_token="$1"
   local wavefront_cluster="$2"
-  local alert_file="$3"
-  local k8s_cluster_name="$4"
+  local k8s_cluster_name="$3"
+  local alert_file="$4"
   local alert_target="$5"
   local alert_name response res_code
 
@@ -61,10 +84,7 @@ function post_alert_to_wavefront() {
     print_err_and_exit "Unable to create alert: $(cat "${response}")"
   fi
 
-  local alert_id
-  alert_id=$(sed -n 's/.*"id":"\{0,1\}\([0-9][^,"]*\)"\{0,1\}.*/\1/p' "${response}")
-
-  echo "Alert has been created at: https://${wavefront_cluster}.wavefront.com/alerts/${alert_id}"
+  echo "Alert has been created."
 }
 
 function get_csp_access_token() {
@@ -156,11 +176,10 @@ function print_usage_and_exit() {
 }
 
 function print_usage() {
-  echo "Usage: create-alert [flags] [options]"
+  echo "Usage: create-all-alerts [flags] [options]"
   echo -e "\t-t wavefront api token (optional)"
   echo -e "\t-c wavefront instance name (required)"
   echo -e "\t-n kubernetes cluster name (required)"
-  echo -e "\t-f alert template file name (required)"
   echo -e "\t-p end-point for csp authentication (optional)"
   echo -e "\t-a api token for csp authentication (optional)"
   echo -e "\t-i oauth app id for csp authentication (optional)"
@@ -173,7 +192,6 @@ function print_usage() {
 function main() {
   # Required arguments
   local WF_CLUSTER=''
-  local ALERT_FILE_NAME=''
   local K8S_CLUSTER_NAME=''
 
   # Optional arguments
@@ -188,12 +206,11 @@ function main() {
   local GIT_BRANCH='main'
   local ALERT_TARGET=''
 
-  while getopts ':t:c:n:f:p:a:i:s:o:e:d:b:h' opt; do
+  while getopts ':t:c:n:p:a:i:s:o:e:d:b:h' opt; do
     case "${opt}" in
       t) WAVEFRONT_TOKEN="${OPTARG}" ;;
       c) WF_CLUSTER="${OPTARG}" ;;
       n) K8S_CLUSTER_NAME="${OPTARG}" ;;
-      f) ALERT_FILE_NAME="${OPTARG}" ;;
       p) CSP_ENDPOINT="${OPTARG}" ;;
       a) CSP_API_TOKEN="${OPTARG}" ;;
       i) CSP_APP_ID="${OPTARG}" ;;
@@ -218,14 +235,12 @@ function main() {
   check_required_argument "${WAVEFRONT_TOKEN}" "-t <WAVEFRONT_TOKEN> is required"
   check_required_argument "${WF_CLUSTER}" "-c <WF_CLUSTER> is required"
   check_required_argument "${K8S_CLUSTER_NAME}" "-n <K8S_CLUSTER_NAME> is required"
-  check_required_argument "${ALERT_FILE_NAME}" "-f <ALERT_FILE_NAME> is required"
 
-  # Download and create the alert
-  TEMP_FILE=$(mktemp)
-  download_alert "${GITHUB_REPO}" "${ALERTS_DIRECTORY}/${ALERT_FILE_NAME}" "${GIT_BRANCH}" "${TEMP_FILE}"
-  check_alert_file "${TEMP_FILE}"
-  post_alert_to_wavefront "${WAVEFRONT_TOKEN}" "${WF_CLUSTER}" "${TEMP_FILE}" "${K8S_CLUSTER_NAME}" "${ALERT_TARGET}"
-  rm "${TEMP_FILE}"
+  # Download and create all the alerts
+  TEMP_DIR=$(mktemp -d)
+  download_alerts "${GITHUB_REPO}" "${ALERTS_DIRECTORY}" "${GIT_BRANCH}"
+  create_alerts "${WAVEFRONT_TOKEN}" "${WF_CLUSTER}" "${K8S_CLUSTER_NAME}" "${ALERT_TARGET}"
+  rm -rf "${TEMP_DIR}"
 }
 
 main "$@"
