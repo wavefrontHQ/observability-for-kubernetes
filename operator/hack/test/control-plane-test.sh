7e4e5e6a2515c713b9f10ed9d538597d45058060
Yuqi Jin <jyuqi@vmware.com>
2023-10-23 13:35:59 -0700

    add type tag for cadvisor and etcd metrics and cleanup
    Signed-off-by: John Cornish <jcornish@vmware.com>
    Co-authored-by: John Cornish <jcornish@vmware.com>

--- operator/hack/test/control-plane-test.sh
+++ operator/hack/test/control-plane-test.sh
@@ -1,6 +1,7 @@
+ETCD_HAS_LEADER_TYPE="cluster"
 exit_on_fail \
   wait_for_query_match_exact \
-  "ts(kubernetes.controlplane.etcd.server.has.leader.gauge%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22)" \
+  "ts(kubernetes.controlplane.etcd.server.has.leader.gauge%2C%20cluster%3D%22${CONFIG_CLUSTER_NAME}%22%20AND%20type%3D%22${ETCD_HAS_LEADER_TYPE}%22)" \
   "1"
 
 num_nodes=$(kubectl get nodes --no-headers | wc -l | tr -d '[:space:]')
