9e0b44d093dc48f0cf0b1de208702d0fe1ac4d06
Jerry Belmonte <bjerry@vmware.com>
2023-11-02 15:33:37 -0700

    Add metric tag filtering to operator config (#422)

--- operator/controllers/wavefront_controller_test.go
+++ operator/controllers/wavefront_controller_test.go
@@ -472,7 +472,7 @@ func TestReconcileCollector(t *testing.T) {
 		require.False(t, mockKM.AppliedContains("v1", "ConfigMap", "wavefront", "collector", "openshift-coredns-control-plane-config"))
 	})
 
-	t.Run("can add custom filters", func(t *testing.T) {
+	t.Run("can add custom metric filters", func(t *testing.T) {
 		r, mockKM := componentScenario(wftest.CR(func(w *wf.Wavefront) {
 			w.Spec.DataCollection.Metrics.Filters.AllowList = []string{"allowSomeTag", "allowOtherTag"}
 			w.Spec.DataCollection.Metrics.Filters.DenyList = []string{"denyAnotherTag", "denyThisTag"}
@@ -486,7 +486,25 @@ func TestReconcileCollector(t *testing.T) {
 		require.True(t, mockKM.CollectorConfigMapContains("metricDenyList:\\n\n    \\   - denyAnotherTag\\n    - denyThisTag"))
 	})
 
-	t.Run("can add custom filter with tag guarantee list", func(t *testing.T) {
+	t.Run("can add custom metric tag filters", func(t *testing.T) {
+		r, mockKM := componentScenario(wftest.CR(func(w *wf.Wavefront) {
+			w.Spec.DataCollection.Metrics.Filters.TagAllowList = map[string][]string{"env1": {"prod", "staging"}}
+			w.Spec.DataCollection.Metrics.Filters.TagDenyList = map[string][]string{"env2": {"test"}}
+			w.Spec.DataCollection.Metrics.Filters.TagInclude = []string{"includeSomeTag"}
+			w.Spec.DataCollection.Metrics.Filters.TagExclude = []string{"excludeSomeTag"}
+		}), nil)
+
+		_, err := r.Reconcile(context.Background(), defaultRequest())
+
+		require.NoError(t, err)
+
+		require.True(t, mockKM.CollectorConfigMapContains("metricTagAllowList:", "env1:", "- prod", "- staging"))
+		require.True(t, mockKM.CollectorConfigMapContains("metricTagDenyList:", "env2:", "- test"))
+		require.True(t, mockKM.CollectorConfigMapContains("tagInclude:", "includeSomeTag"))
+		require.True(t, mockKM.CollectorConfigMapContains("tagExclude:", "excludeSomeTag"))
+	})
+
+	t.Run("can add custom metric filter with tag guarantee list", func(t *testing.T) {
 		r, mockKM := componentScenario(wftest.CR(func(w *wf.Wavefront) {
 			w.Spec.DataCollection.Metrics.Filters.TagGuaranteeList = []string{"someTagToAlwaysProtect", "someOtherTagToAlwaysProtect"}
 		}), nil)
@@ -1589,7 +1607,7 @@ func TestReconcileInsightsByCR(t *testing.T) {
 
 		require.True(t, mockKM.CollectorConfigMapContains("externalEndpointURL: \\\"https://example.com\\\""))
 		require.True(t, mockKM.CollectorConfigMapContains("enableEvents:\n    false"))
-		require.True(t, mockKM.CollectorConfigMapContains("type: \\\"external\\\"\\n  enableEvents: true"))
+		require.True(t, mockKM.CollectorConfigMapContains("type: \\\"external\\\"", "enableEvents: true"))
 		require.True(t, mockKM.CollectorConfigMapContains("events:\\n\n    \\ filters:\\n    tagAllowListSets:\\n    - type:\\n      - \\\"Warning\\\"\\n    - type:\\n\n    \\     - \\\"Normal\\\"\\n      kind:\\n      - \\\"Pod\\\"\\n      reason:\\n      - \\\"Backoff\\\""))
 		require.True(t, mockKM.CollectorConfigMapContains("proxyAddress: wavefront-proxy:2878"))
 
