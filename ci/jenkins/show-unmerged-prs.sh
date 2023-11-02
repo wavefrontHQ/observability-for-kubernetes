5e956f071ae24c4c4a179b018c58bf53eb1cca30
John Cornish <jcornish@vmware.com>
2023-11-02 09:26:29 -0600

    Show unmerged JIRA PRs

--- ci/jenkins/show-unmerged-prs.sh
+++ ci/jenkins/show-unmerged-prs.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-OPEN_STORY_PRS=$(curl -sSL \
-    -X GET \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/wavefrontHQ/observability-for-kubernetes/pulls?state=open \
-    | jq -r .[].head.ref \
-    | grep K8SSAAS-)
+echo "Unmerged JIRA story PRs:"
+echo "------------------------"
 
-echo "Unmerged PR branches:"
-for open_story_pr in ${OPEN_STORY_PRS} ; do
-  echo "- ${open_story_pr}: https://github.com/wavefrontHQ/observability-for-kubernetes/tree/${open_story_pr}"
-done
+curl -sSL \
+  -X GET \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/wavefrontHQ/observability-for-kubernetes/pulls?state=open \
+  | jq -r '.[] | select(.head.ref | startswith("K8SSAAS-")) | {ref: .head.ref, url: .html_url}' \
+  | jq -r '"\(.url) | \(.ref)"'
