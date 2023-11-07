b6da7c6282dac632d702c3fb3da2c5affde59ed0
mamichael <mamichael@vmware.com>
2023-11-07 13:42:36 -0700

    Starting of event matchers
    Co-authored-by: Yuqi Jin <jyuqi@vmware.com>

--- collector/plugins/events/eventannotater.go
+++ collector/plugins/events/eventannotater.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/wavefronthq/observability-for-kubernetes/collector/internal/util"
 	v1 "k8s.io/api/core/v1"
+	corev1listers "k8s.io/client-go/listers/core/v1"
 )
 
 // category
@@ -27,6 +28,99 @@ const (
 	FailedCreate          = "FailedCreate"
 	Terminating           = "Terminating"
 )
+
+type match func(event *v1.Event) bool
+type eventMatcher struct {
+	match       match
+	category    string
+	subcategory string
+	podLister   corev1listers.PodLister
+}
+
+type eventAnnotater struct {
+	workloadCache util.WorkloadCache
+	clusterName   string
+	clusterUUID   string
+	eventMatchers []eventMatcher
+}
+
+func (em *eventMatcher) matches(event *v1.Event) bool {
+	return em.match(event)
+}
+
+func NewEventAnnotator(workloadCache util.WorkloadCache, clusterName, clusterUUID string) eventAnnotater {
+	matchers := make([]eventMatcher, 5)
+
+	imageMatcher := eventMatcher{
+		match: func(event *v1.Event) bool {
+			return event.Reason == "Failed" && strings.Contains(strings.ToLower(event.Message), "image")
+		},
+		category:    Creation,
+		subcategory: ImagePullBackOff,
+		podLister:   nil,
+	}
+	matchers = append(matchers, imageMatcher)
+
+	backoffMatcher := eventMatcher{
+		match: func(event *v1.Event) bool {
+			return event.Reason == "BackOff" && strings.Contains(event.Message, "Back-off restarting")
+		},
+		category:    Runtime,
+		subcategory: CrashLoopBackOff,
+		podLister:   nil,
+	}
+
+	matchers = append(matchers, backoffMatcher)
+
+	failedMount := eventMatcher{
+		match: func(event *v1.Event) bool {
+			return event.Reason == "FailedMount"
+		},
+		category:    Creation,
+		subcategory: FailedMount,
+		podLister:   nil,
+	}
+
+	unhealthy := eventMatcher{
+		match: func(event *v1.Event) bool {
+			return event.Reason == "Unhealthy"
+		},
+		category:    Runtime,
+		subcategory: Unhealthy,
+		podLister:   nil,
+	}
+
+	matchers = append(matchers, unhealthy)
+
+	scheduling := eventMatcher{
+		match: func(event *v1.Event) bool {
+			return event.Reason == "FailedScheduling" && strings.Contains(strings.ToLower(event.Message), "insufficient")
+		},
+		category:    Scheduling,
+		subcategory: InsufficientResources,
+		podLister:   nil,
+	}
+
+	matchers = append(matchers, scheduling)
+
+	failedCreate := eventMatcher{
+		match: func(event *v1.Event) bool {
+			return event.Reason == "FailedCreate"
+		},
+		category:    Scheduling,
+		subcategory: InsufficientResources,
+		podLister:   nil,
+	}
+
+	matchers = append(matchers, failedCreate)
+
+	return eventAnnotater{
+		workloadCache: workloadCache,
+		clusterName:   clusterName,
+		clusterUUID:   clusterUUID,
+		eventMatchers: matchers,
+	}
+}
 
 func annotateEvent(event *v1.Event, workloadCache util.WorkloadCache, clusterName, clusterUUID string) {
 	if event.ObjectMeta.Annotations == nil {
