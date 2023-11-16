85ae5e8d71fbf8fade1c0205e5e73bb703fa8166
Devon Warshaw <warshawd@vmware.com>
2023-11-16 10:57:39 -0700

    finish rbac generation
    Signed-off-by: Matthew Conger-Eldeen <matthewco@vmware.com>

--- operator/components/pixie/preprocess/common-rbac.sh
+++ operator/components/pixie/preprocess/common-rbac.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -eup pipefail
 
+function update_subject_if_necessary() {
+    local input="$(</dev/stdin)"
+    if [[ "$(yq '. | has("subjects")' <<< "$input")" == "true" ]]; then
+      yq '(.subjects[] | .namespace) = "observability-system"' <<< "$input"
+    else
+      echo "$input"
+    fi
+}
+
 yq '.metadata.namespace = "observability-system"' <&0 |
   yq '.metadata.labels["app.kubernetes.io/name"] = "wavefront"' |
-  yq '.metadata.labels["app.kubernetes.io/component"] = "pixie"'
+  yq '.metadata.labels["app.kubernetes.io/component"] = "pixie"' |
+  update_subject_if_necessary
