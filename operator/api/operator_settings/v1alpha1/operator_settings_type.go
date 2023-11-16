f4c50212efa9eaf6142c4304088f0a8f1229c441
Matthew Conger-Eldeen <matthewco@vmware.com>
2023-11-16 16:17:15 -0700

    add OperatorSettingsList
    Signed-off-by: Devon Warshaw <warshawd@vmware.com>

--- operator/api/operator_settings/v1alpha1/operator_settings_type.go
+++ operator/api/operator_settings/v1alpha1/operator_settings_type.go
@@ -2,6 +2,18 @@ package v1alpha1
 
 import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+func init() {
+	SchemeBuilder.Register(&OperatorSettings{}, &OperatorSettingsList{})
+}
+
+// +kubebuilder:object:root=true
+// OperatorSettingsList contains a list of OperatorSettings
+type OperatorSettingsList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []OperatorSettings `json:"items"`
+}
+
 // +kubebuilder:object:root=true
 // OperatorSettings is the Schema for the operator API
 type OperatorSettings struct {
