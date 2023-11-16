68ae1be8fd786390714252b8e93f6e488ddeef39
Devon Warshaw <warshawd@vmware.com>
2023-11-16 11:55:24 -0700

    add CRSet and SpecSet
    Signed-off-by: Matthew Conger-Eldeen <matthewco@vmware.com>

--- operator/api/cr_set.go
+++ operator/api/cr_set.go
@@ -1,1 +1,23 @@
 package api
+
+import (
+	ops "github.com/wavefronthq/observability-for-kubernetes/operator/api/operator_settings/v1alpha1"
+	wf "github.com/wavefronthq/observability-for-kubernetes/operator/api/wavefront/v1alpha1"
+)
+
+type CRSet struct {
+	wf.Wavefront
+	ops.OperatorSettings
+}
+
+func (c *CRSet) Spec() *SpecSet {
+	return &SpecSet{
+		WavefrontSpec:       c.Wavefront.Spec,
+		OperatorSettingSpec: c.OperatorSettings.Spec,
+	}
+}
+
+type SpecSet struct {
+	wf.WavefrontSpec
+	ops.OperatorSettingSpec
+}
